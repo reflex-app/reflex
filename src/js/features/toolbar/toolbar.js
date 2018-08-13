46a09cdd011b5a26906da90e3b265de7c4e5cfd0
Nick Wittwer <nick.wittwer@me.com>
2018-08-13 23:41:57 +0200

    postMessage from webview to parent is working

--- src/js/features/toolbar/toolbar.js
+++ src/js/features/toolbar/toolbar.js
@@ -18,30 +18,29 @@ app.toolbar = {
         var url_val = $("#toolbar__url").val();
         var webview = $("webview");
 
-        // Update webview src
-        webview.attr("src", url_val);
+        if ( nw ) { 
+            webview.each(function () {
+                // Update webview src
+                $(this).attr("src", url_val);
+    
+                // Show loading spinner
+                $(this).on("loadstart", function () {
+                    app.toolbar.isLoading(true, url_val)
+                });
+    
+                // Site is loaded
+                $(this).on("loadstop", function () {
+                    // Hide loading spinner
+                    app.toolbar.isLoading(false, url_val);
+                    // Unregister the load event of any previous webviews
+                    $(this).off("loadstart loadstop");
+                    // Setup the event listener on the child
+                    app.toolbar.handleWebviewChild($(this)[0]);
+                });
+            });
+        } else {
 
-        // Add loader
-        // app.toolbar.isLoading(true, url_val);
-
-        // Once they're loaded
-        // webview.on("load", function () {
-        //     app.toolbar.isLoading(false, url_val);
-        //     // Unregister the load event of any previous webviews
-        //     $(this).off("load");
-        // });
-
-        webview.on("loadstart", function () {
-            app.toolbar.isLoading(true, url_val)
-        });
-        webview.on("loadstop", function () {
-            app.toolbar.isLoading(false, url_val);
-            // Unregister the load event of any previous webviews
-            $(this).off("loadstart loadstop");
-
-            // Setup the event listener on the child
-            app.toolbar.handleWebviewChild();
-        });
+        }
     },
 
     isLoading: function (condition, url) {
@@ -58,18 +57,58 @@ app.toolbar = {
         }
     },
 
-    handleWebviewChild: function() {
-        //  // Inject click handler
-        //  var webview_js = document.querySelector('webview');
-        //  webview_js.executeScript({
-        //     //  mainWorld: true,
-        //      code: 'document.querySelector("a").onclick = function() { window.parent.document.postMessage("Hello World!"); }'
-        //  });
+    handleWebviewChild: function ($el) {
 
-        //  window.addEventListener('message', function(e) {
-        //     var message = e.data;
-        //     console.log(message);
-        //   });
+        // Listen for events
+        window.addEventListener('message', function (e) {
+            console.log("Returned message:" + e.data );
+        });
+
+        // Send a message to establish connection
+        try {
+            $el.contentWindow.postMessage('hi', '*');
+            console.log("message sent to webview");
+        } catch(e) {
+            console.log('message to webview failed');
+        }
+
+        // Run on trigger
+        app.toolbar.injectWebviewChildScript($el);
+
+        // a link was clicked, and a new page loaded...
+        // inject the link clicking tracker again
+        var all_webviews = document.querySelector('webview');
+        window.addEventListener('webviewLinkClicked', app.toolbar.injectWebviewChildScript(all_webviews));
+    },
+
+    injectWebviewChildScript: function ($el) {
+        console.log('Injected');
+
+        // Inject a script into the webview to track click events and propagate it to the parent
+        // Make sure to run this function again after click (re-inject)
+        var injection_code = `
+        window.addEventListener('message', function (e) {
+            e.source.postMessage("hello there", "*");
+
+            document.querySelector("a").onclick = function (r) {
+                e.source.postMessage(r, "*");
+            }
+        })
+
+        window.onbeforeunload = function () {
+            parent.postMessage("hello there", "*");
+            document.body.style.backgroundColor = "green";
+        }`;
+
+        // document.querySelector("a").onclick = function () {
+        //     window.parent.window.parent.document.trigger('webviewLinkClicked');
+        //     document.body.style.backgroundColor = "pink";
+        // }
+
+        // Inject click handler
+        $el.executeScript({
+            code: injection_code
+        });
     }
 
 }
