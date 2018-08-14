dea738e9982cf788df5ecfd8528743488bab5aaa
Nick Wittwer <nick.wittwer@me.com>
2018-08-14 19:47:39 +0200

    (WIP) Added WebView communication and JS injection

--- src/js/features/toolbar/toolbar.js
+++ src/js/features/toolbar/toolbar.js
@@ -18,16 +18,16 @@ app.toolbar = {
         var url_val = $("#toolbar__url").val();
         var webview = $("webview");
 
-        if ( nw ) { 
+        if (nw) {
             webview.each(function () {
                 // Update webview src
                 $(this).attr("src", url_val);
-    
+
                 // Show loading spinner
                 $(this).on("loadstart", function () {
                     app.toolbar.isLoading(true, url_val)
                 });
-    
+
                 // Site is loaded
                 $(this).on("loadstop", function () {
                     // Hide loading spinner
@@ -59,51 +59,45 @@ app.toolbar = {
 
     handleWebviewChild: function ($el) {
 
+        // Run on trigger
+        app.toolbar.injectWebviewChildScript($el);
+
         // Listen for events
         window.addEventListener('message', function (e) {
-            console.log("Returned message:" + e.data );
+            console.log("Returned message:" + e.data);
+            // Inject the script again
+            app.toolbar.updateURL();
         });
 
         // Send a message to establish connection
         try {
             $el.contentWindow.postMessage('hi', '*');
             console.log("message sent to webview");
-        } catch(e) {
+        } catch (e) {
             console.log('message to webview failed');
         }
-
-        // Run on trigger
-        app.toolbar.injectWebviewChildScript($el);
-
-        // a link was clicked, and a new page loaded...
-        // inject the link clicking tracker again
-        var all_webviews = document.querySelector('webview');
-        window.addEventListener('webviewLinkClicked', app.toolbar.injectWebviewChildScript(all_webviews));
     },
 
     injectWebviewChildScript: function ($el) {
-        console.log('Injected');
+        console.log('Link click script injected');
 
         // Inject a script into the webview to track click events and propagate it to the parent
         // Make sure to run this function again after click (re-inject)
         var injection_code = `
         window.addEventListener('message', function (e) {
-            e.source.postMessage("hello there", "*");
-
             document.querySelector("a").onclick = function (r) {
-                e.source.postMessage(r, "*");
+                e.source.postMessage("hello there", "*");
             }
-        })
+        })`;
 
-        window.onbeforeunload = function () {
-            parent.postMessage("hello there", "*");
-            document.body.style.backgroundColor = "green";
-        }`;
-
-        // document.querySelector("a").onclick = function () {
-        //     window.parent.window.parent.document.trigger('webviewLinkClicked');
-        //     document.body.style.backgroundColor = "pink";
-        // }
+        // $el.addContentScripts([{
+        //     name: 'myRule',
+        //     matches: ['*'],
+        //     js: {
+        //         files: ['']
+        //     },
+        //     run_at: 'document_start'
+        // }]);
 
         // Inject click handler
         $el.executeScript({
