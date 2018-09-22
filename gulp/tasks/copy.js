d15d21f3c8bac20e58c33ee01d485aceb0936f3f
Nick Wittwer <nick.wittwer@me.com>
2018-09-22 09:51:01 +0200

    Updated copy paths

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp');
 const CONFIG = require('../config.js');
 
+gulp.task('copy', gulp.series('copy:assets', 'copy:node-modules'));
+
 // Copies static assets
-gulp.task('copy', function() {
+gulp.task('copy:assets', function() {
   return gulp.src(CONFIG.ASSETS_FILES)
     .pipe(gulp.dest(CONFIG.DIST + 'assets'));
 });
+
+// Copies Node_Modules folder
+gulp.task('copy:node-modules', function() {
+  return gulp.src(CONFIG.SRC + 'node_modules/**/*')
+    .pipe(gulp.dest(CONFIG.DIST + 'node_modules/'));
+});
+
