159702d091858d45fbbb116fda9d65d4b11177f5
Nick Wittwer <nick.wittwer@me.com>
2018-10-03 11:41:44 +0200

    Wrapped nw-specific settings function with conditional to prevent errors

--- src/js/features/settings/settings.js
+++ src/js/features/settings/settings.js
@@ -1,36 +1,37 @@
 app.settings = {
-    init: function() {
+    init: function () {
         // Start the artboard sizes
         app.settings.artboardSizes.init();
     },
 
-    firstLoad: function() {
-    }
+    firstLoad: function () {}
 }
 
 //
 // Custom menubar
-//
-// Create menu container
-var Menu = new nw.Menu({
-    type: 'menubar'
-});
-//initialize default mac menu
-Menu.createMacBuiltin(nw.App.manifest.name);
-// Get the root menu from the default mac menu
-var rootMenu = Menu.items[0].submenu;
-// Append new item to root menu
-rootMenu.insert(
-    new nw.MenuItem({
-        label: 'Preferences',
-        click: function () {
-            toggleSettings();
-        }
-    }), 1 // <-- 0-index where you want to insert
-);
-// Append Menu to Window
-nw.Window.get().menu = Menu;
 
+var nw;
+if (nw) {
+    // Create menu container
+    var Menu = new nw.Menu({
+        type: 'menubar'
+    });
+    //initialize default mac menu
+    Menu.createMacBuiltin(nw.App.manifest.name);
+    // Get the root menu from the default mac menu
+    var rootMenu = Menu.items[0].submenu;
+    // Append new item to root menu
+    rootMenu.insert(
+        new nw.MenuItem({
+            label: 'Preferences',
+            click: function () {
+                toggleSettings();
+            }
+        }), 1 // <-- 0-index where you want to insert
+    );
+    // Append Menu to Window
+    nw.Window.get().menu = Menu;
+}
 
 // Click function
 function toggleSettings() {
