6cf69b5d1f55d45973908d4abd90f2c1fba0f5cc
Nick Wittwer <nick.wittwer@me.com>
2018-10-21 20:23:42 +0200

    Added Settings menu

--- src/js/features/settings/settings-artboardSizes.js
+++ src/js/features/settings/settings-artboardSizes.js
@@ -1,88 +1,87 @@
+// @TODO: This should probably live in a `data` directory
+
+const $ = require('jquery');
+
 // 1. check/read localstorage
 // when you open it
 // 2. add/update localstorage
 // when you change a size
-app.settings.artboardSizes = {
-    localStorageKey: "artboardSizes",
-    localStorageObject: [],
 
-    init: function () {
-        app.settings.artboardSizes.firstLoad();
-    },
+export function artboardSizes(app) {
+    let localStorageKey = "artboardSizes";
+    let localStorageObject = [];
 
-    firstLoad: function () {
-        // Initial check if there's existing data
-        // If so, show it
-        // If not, create some
-        app.settings.artboardSizes.readLocalStorage();
-    },
+    // Initial check if there's existing data
+    // If so, show it
+    // If not, create some
+    readLocalStorage();
 
     /**
      * Read the localStorage
      * If nothing exists, create the key with the current artboard sizes
      */
-    readLocalStorage: function () {
+    function readLocalStorage() {
         // Return the localStorage
-        if (localStorage[this.localStorageKey] == null || !localStorage[this.localStorageKey]) {
+        if (localStorage[localStorageKey] == null || !localStorage[localStorageKey]) {
             // Nothing is defined!
             if (app.environment == "dev") {
                 console.log('localStorage: No Artboard sizes exist');
             }
-            app.settings.artboardSizes.createLocalStorage();
+            createLocalStorage();
         } else {
             // It's defined!
             // Use the saved sizes
             if (app.environment == "dev") {
                 console.log('localStorage: Artboard sizes exists');
             }
-            app.settings.artboardSizes.restorePreviousFromLocalStorage();
+            restorePreviousFromLocalStorage();
         }
-    },
+    }
 
     // Update our local variable
-    updateLocalStorageObject: function () {
-        if (localStorage.getItem(this.localStorageKey)) {
-            this.localStorageObject = JSON.parse(localStorage.getItem(this.localStorageKey));
-            return this.localStorageObject;
+    function updateLocalStorageObject() {
+        if (localStorage.getItem(localStorageKey)) {
+            localStorageObject = JSON.parse(localStorage.getItem(localStorageKey));
+            return localStorageObject;
         } else {
             if (app.environment == "dev") {
                 console.log('No local storage exists')
             }
         }
-    },
+    }
 
     /**
      * Initializes the localStorage definition
      */
-    createLocalStorage: function () {
+    function createLocalStorage() {
         // No localStorage exists!
         // Lets setup an object with the current artboard sizes
-        this.localStorageObject.push(app.artboard.dimensions.return());
+        localStorageObject.push(app.artboard.dimensions.return());
 
         // Now create a new key and save to it
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.localStorageObject));
+        localStorage.setItem(localStorageKey, JSON.stringify(localStorageObject));
 
         // Update our local variable
-        app.settings.artboardSizes.updateLocalStorageObject();
+        updateLocalStorageObject();
 
         // Add to LocalStorage
         if (app.environment == "dev") {
-            console.log('Added to localstorage: ', app.settings.artboardSizes.updateLocalStorageObject());
+            console.log('Added to localstorage: ', updateLocalStorageObject());
         }
-    },
+    }
 
     /**
      * Restores the artboard sizes from the existing localStorage data
      */
-    restorePreviousFromLocalStorage: function () {
+    function restorePreviousFromLocalStorage() {
         // Update our local variable
-        app.settings.artboardSizes.updateLocalStorageObject();
+        updateLocalStorageObject();
 
         // Remove the initial frames, and load the old ones
         $(artboards).empty();
 
         // Now create the artboards from the last save state
-        var object = app.settings.artboardSizes.localStorageObject[0];
+        var object = localStorageObject[0];
 
         for (var key in object) {
             if (object.hasOwnProperty(key)) {
@@ -92,35 +91,34 @@ app.settings.artboardSizes = {
                 // if ( app.environment == "dev" ) { console.log(key, instance); }
             }
         }
-    },
+    }
 
     /**
      * Updates the existing localStorage definition with the sizes of the current artboards
      */
-    updateLocalStorage: function () {
+    function updateLocalStorage() {
         // Add to existing
-        if (this.localStorageObject.length >= 1 && this.localStorageObject.length !== null) {
+        if (localStorageObject.length >= 1 && localStorageObject.length !== null) {
             // localStorage key exists!
             // Clear the existing object
-            this.localStorageObject = [];
+            localStorageObject = [];
 
             // Lets setup an object with the current artboard sizes
-            this.localStorageObject.push(app.artboard.dimensions.return());
+            localStorageObject.push(app.artboard.dimensions.return());
 
             // Now update the localStorage, with only one save state
-            localStorage.setItem(this.localStorageKey, JSON.stringify(this.localStorageObject));
+            localStorage.setItem(localStorageKey, JSON.stringify(localStorageObject));
 
             // Update our local variable
-            app.settings.artboardSizes.updateLocalStorageObject();
+            updateLocalStorageObject();
         } else {
             // @TODO Add a UI alert for this error
             console.log('There was a problem updating localStorage.');
         }
 
         // Log event when localStorage has been updated
-        if ( app.environment == "dev" ) {
-            console.log('Added Artboard(s) to localstorage: ', app.settings.artboardSizes.updateLocalStorageObject()[0]);
+        if (app.environment == "dev") {
+            console.log('Added Artboard(s) to localstorage: ', updateLocalStorageObject()[0]);
         }
     }
-
 }
--- src/js/features/settings/settings.js
+++ src/js/features/settings/settings.js
@@ -1,63 +1,74 @@
-app.settings = {
-    init: function () {
-        // Start the artboard sizes
-        app.settings.artboardSizes.init();
-    },
+const $ = require('jquery');
 
-    firstLoad: function () {}
-}
+import { artboardSizes } from "../settings/settings-artboardSizes";
 
-//
-// Custom Mac menubar
-//	
-if (app.platform == "native") {
+export class Settings {
+    constructor(app) {
+        // @TODO: Add the rest of the artboard functionality (i.e. artboard.add())
+        // Retrieve localStorage
+        // artboardSizes(this);
 
-    // Make the NW window object accessible
-    let win = nw.Window.get();
-
-    // Create menu container
-    var Menu = new nw.Menu({
-        type: 'menubar'
-    });
-
-    //initialize default mac menu
-    Menu.createMacBuiltin(nw.App.manifest.name);
-
-    // Get the root menu from the default mac menu
-    var rootMenu = Menu.items[0].submenu;
-
-    // Append new item to root menu
-    rootMenu.insert(
-        new nw.MenuItem({
-            label: 'Preferences',
-            click: function () {
-                toggleSettings();
-            }
-        }), 1 // <-- 0-index where you want to insert
-    );
-
-    // Append Menu to Window
-    win.menu = Menu;
-}
-
-// Click function
-function toggleSettings() {
-    var $el = $("#settings");
-    var settingsClass = "is-visible";
-
-    if ($el.hasClass(settingsClass)) {
-        $el.removeClass(settingsClass);
-        // Unbind
-        $("body").off('click', clickOutside);
-    } else {
-        $el.addClass(settingsClass);
-        // Bind 
-        $("body").on('click', clickOutside);
+        if (app.platform == "native") {
+            this.createMacSettingsMenu();
+        }
     }
+
+    // Create Mac Menu
+    createMacSettingsMenu() {
+        // Make the NW window object accessible
+        let win = nw.Window.get();
+
+        // Create menu container
+        var Menu = new nw.Menu({
+            type: 'menubar'
+        });
+
+        //initialize default mac menu
+        Menu.createMacBuiltin(nw.App.manifest.name);
+
+        // Get the root menu from the default mac menu
+        var rootMenu = Menu.items[0].submenu;
+
+        // Append new item to root menu
+        rootMenu.insert(
+            new nw.MenuItem({
+                label: 'Preferences',
+                click: function () {
+                    var $el = $("#settings");
+                    var settingsClass = "is-visible";
+
+                    function clickOutside(event) {
+                        if (!$(event.target).closest('.settings__inner').length && !$(event.target).is('.settings__inner')) {
+                            toggleSettings();
+                        }
+                    }
+
+                    function toggleSettings() {
+                        if ($el.hasClass(settingsClass)) {
+                            $el.removeClass(settingsClass);
+                            // Unbind
+                            $("body").off('click', clickOutside);
+                        } else {
+                            $el.addClass(settingsClass);
+                            // Bind 
+                            $("body").on('click', clickOutside);
+                        }
+                    }
+
+                    // Initialize
+                    toggleSettings();
+                }
+            }), 1 // <-- 0-index where you want to insert
+        );
+
+        // Append Menu to Window
+        win.menu = Menu;
+    }
+
 }
 
-function clickOutside(event) {
-    if (!$(event.target).closest('.settings__inner').length && !$(event.target).is('.settings__inner')) {
-        toggleSettings();
-    }
-}
+// app.settings = {
+//     init: function () {
+//         // Start the artboard sizes
+//         app.settings.artboardSizes.init();
+//     },
