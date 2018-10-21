67a102c3924cb894fc1949e10385f7195a3457de
Nick Wittwer <nick.wittwer@me.com>
2018-10-21 22:45:11 +0200

    Added dimensions es6

--- src/js/features/artboard/artboard-dimensions.js
+++ src/js/features/artboard/artboard-dimensions.js
@@ -1,72 +1,144 @@
-app.artboard.dimensions = {
+const $ = require('jquery');
 
-    init: function () {
-        this.firstLoad();
-    },
+let artboard = $('.artboard'); // TODO: Refactor this to a more re-usable place
 
-    firstLoad: function () {
-        app.artboard.dimensions.update(artboard)
-    },
+export function updateDimensions($el, width, height) {
 
-    /**
-     * Function: Resize
-     * @param  {} $el
-     * @param  {} width
-     * @param  {} height
-     */
-    update: function ($el, width, height) {
-        if ( width || height ) {
-            var height_container = $el.closest(artboard).find('.artboard__height');
-                var width_container = $el.closest(artboard).find('.artboard__width');
-    
-                if (typeof width !== undefined) {
-                    width = $el.closest(artboard).width();
-                    width_container.html(width + "px");
-                    // console.log(width);
-                }
-    
-                if (typeof height !== undefined) {
-                    height = $el.closest(artboard).height();
-                    height_container.html(height + "px");
-                    // console.log(height);
-                }
-        } else {
-            $.each($el, function () {
-                var height_container = $(this).closest(artboard).find('.artboard__height');
-                var width_container = $(this).closest(artboard).find('.artboard__width');
-    
-                if (typeof width !== undefined) {
-                    width = $(this).closest(artboard).width();
-                    width_container.html(width + "px");
-                    // console.log(width);
-                }
-    
-                if (typeof height !== undefined) {
-                    height = $(this).closest(artboard).height();
-                    height_container.html(height + "px");
-                    // console.log(height);
-                }
-    
-                // console.log($(this));
-    
-            });
+    // Updates the dimensions of a given artboard (via jQuery element)
+    if (width || height) {
+        var height_container = $el.closest(artboard).find('.artboard__height');
+        var width_container = $el.closest(artboard).find('.artboard__width');
+
+        if (typeof width !== undefined) {
+            width = $el.closest(artboard).width();
+            width_container.html(width + "px");
+            // console.log(width);
         }
-    },
 
-    return: function() {
-        var obj = [];
+        if (typeof height !== undefined) {
+            height = $el.closest(artboard).height();
+            height_container.html(height + "px");
+            // console.log(height);
+        }
+    } else {
+        $.each($el, function () {
+            var height_container = $(this).closest(artboard).find('.artboard__height');
+            var width_container = $(this).closest(artboard).find('.artboard__width');
 
-        $.each( $(".artboard"), function () {
-            var width;
-            var height;
+            if (typeof width !== undefined) {
+                width = $(this).closest(artboard).width();
+                width_container.html(width + "px");
+                // console.log(width);
+            }
 
-            width = $(this).width();
-            height = $(this).height();
+            if (typeof height !== undefined) {
+                height = $(this).closest(artboard).height();
+                height_container.html(height + "px");
+                // console.log(height);
+            }
 
-            obj.push({width, height});
+            // console.log($(this));
+
         });
-
-        return obj;
     }
-
 }
+
+export function returnDimensions() {
+    var obj = [];
+
+    $.each($(".artboard"), function () {
+        var width;
+        var height;
+
+        width = $(this).width();
+        height = $(this).height();
+
+        obj.push({
+            width,
+            height
+        });
+    });
+
+    return obj;
+}
+
+
+// ////////////////////////////
+// ////////////////////////////
+// ////////////////////////////
+
+// app.artboard.dimensions = {
+
+//     init: function () {
+//         this.firstLoad();
+//     },
+
+//     firstLoad: function () {
+//         app.artboard.dimensions.update(artboard)
+//     },
+
+//     /**
+//      * Function: Resize
+//      * @param  {} $el
+//      * @param  {} width
+//      * @param  {} height
+//      */
+//     update: function ($el, width, height) {
+//         if (width || height) {
+//             var height_container = $el.closest(artboard).find('.artboard__height');
+//             var width_container = $el.closest(artboard).find('.artboard__width');
+
+//             if (typeof width !== undefined) {
+//                 width = $el.closest(artboard).width();
+//                 width_container.html(width + "px");
+//                 // console.log(width);
+//             }
+
+//             if (typeof height !== undefined) {
+//                 height = $el.closest(artboard).height();
+//                 height_container.html(height + "px");
+//                 // console.log(height);
+//             }
+//         } else {
+//             $.each($el, function () {
+//                 var height_container = $(this).closest(artboard).find('.artboard__height');
+//                 var width_container = $(this).closest(artboard).find('.artboard__width');
+
+//                 if (typeof width !== undefined) {
+//                     width = $(this).closest(artboard).width();
+//                     width_container.html(width + "px");
+//                     // console.log(width);
+//                 }
+
+//                 if (typeof height !== undefined) {
+//                     height = $(this).closest(artboard).height();
+//                     height_container.html(height + "px");
+//                     // console.log(height);
+//                 }
+
+//                 // console.log($(this));
+
+//             });
+//         }
+//     },
+
+//     return: function () {
+//         var obj = [];
+
+//         $.each($(".artboard"), function () {
+//             var width;
+//             var height;
+
+//             width = $(this).width();
+//             height = $(this).height();
+
+//             obj.push({
+//                 width,
+//                 height
+//             });
+//         });
+
+//         return obj;
+//     }
+
+// }
