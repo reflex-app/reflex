ef07d19828dafabc1dfadcdd13e9a00f9cfa2628
Nick Wittwer <nick.wittwer@me.com>
2018-10-21 20:24:01 +0200

    Moved environment and platform info to Shift instance

--- src/js/index.js
+++ src/js/index.js
@@ -6,48 +6,7 @@ import { shift } from "./shift"; // Load Shift class
 export let app = shift; // Create the main instance
 console.log(app);
 
-///////////////////////////////
-///////////////////////////////
 
-// Check if this a dev or production environment
-let currentURL = new URL(window.location.href);
-if (currentURL.hostname.includes("localhost")) {
-    console.log("Environment: development");
-    app.environment = "dev";
-} else {
-    console.log("Environment: production");
-    app.environment = "production";
-}
-
-// The app is written for the web by default
-// So you can add a !isWeb flag to add native (Mac/Windows) features
-// Checks by seeing if nw (NW.js) is defined
-let nw = window.nw || null;
-
-if (nw == null) {
-    // App is on web
-    app.platform = "web";
-} else {
-    // App is using NW.js (Mac/Windows app)
-    app.platform = "native";
-}
-
-// Native (nw.js) functions
-if (app.platform == "native") {
-    // Make the NW window object accessible
-    let win = nw.Window.get();
-
-    // Open links in the native browser
-    win.on('new-win-policy', function (frame, url, policy) {
-        policy.ignore(); // Do not open using the app window
-        nw.Shell.openExternal(url); // Open in external browser
-    });
-
-    // Log the current version in development
-    if (app.environment == "dev") {
-        console.log("NW.js Version: ", process.versions['node-webkit']);
-    }
-}
 
 ///////////////////////////////
 ///////////////////////////////
--- src/js/shift.js
+++ src/js/shift.js
@@ -1,32 +1,70 @@
 /*
 A Shift instance should contain:
-- methods for artboards, canvas, etc.
 - details about the environment
 - details about the operating system
+- methods for artboards, canvas, etc.
 - list of artboards
 - event listeners
 */
 
-import Canvas from "./features/canvas/canvas";
+import { Canvas } from "./features/canvas/canvas";
+import { Settings } from "./features/settings/settings";
 
 // Create the main Shift class object
 // This controls all details of the overall app
 class Shift {
-    constructor(options) {
+    constructor() {
+        this.environment = this.getEnvironment();
+        this.platform = this.getPlatform();
+
         // Artboards
         this.artboards = {}; // keep track of all the artboards
-        
+
         // Create a canvas
         this.canvas = new Canvas(this);
 
-        // this.artboards = $("#artboards");
-        // this.artboard = $(".artboard");
-        // this.artboardInnerFrame = $("iframe");
+        // Settings
+        this.settings = new Settings(this);
+    }
 
-        // Set initial scale
-        // this.minScaleX = $(window).width() / artboards.innerWidth();
-        // this.minScaleY = $(window).height() / artboards.innerHeight();
-        // Artboard.add();
+    getEnvironment() {
+        // Check if this a dev or production environment
+        let currentURL = new URL(window.location.href);
+        if (currentURL.hostname.includes("localhost")) {
+            console.log("Environment: development");
+            return "dev";
+        } else {
+            console.log("Environment: production");
+            return "production";
+        }
+    }
+
+    getPlatform() {
+        // The app is written for the web by default
+        // So you can add a !isWeb flag to add native (Mac/Windows) features
+        // Checks by seeing if nw (NW.js) is defined
+        let nw = window.nw || null;
+
+        if (nw == null) {
+            // App is on web
+            return "web";
+        } else {
+            // App is using NW.js (Mac/Windows app)
+            let win = nw.Window.get();
+
+            // Open links in the native browser
+            win.on('new-win-policy', function (frame, url, policy) {
+                policy.ignore(); // Do not open using the app window
+                nw.Shell.openExternal(url); // Open in external browser
+            });
+
+            // Log the current version in development
+            if (this.environment == "dev") {
+                console.log("NW.js Version: ", window.process.versions['node-webkit']);
+            }
+
+            return "native";
+        }
     }
 }
 
