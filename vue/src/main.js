bee17c097993d797b5372b11c0ac0d5f5704f392
Nick Wittwer <nick.wittwer@me.com>
2018-10-27 20:26:09 +0200

    Added VueX

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
+import store from './store/index'
 import App from './App.vue'
 
+// Vue Configuration
 Vue.config.productionTip = false
 
+// Create the <App> Vue instance
 new Vue({
+  el: "#app",
+  store,
   render: h => h(App)
-}).$mount('#app')
+})
 
 // Create a moveable canvas
 import panzoom from "panzoom";
