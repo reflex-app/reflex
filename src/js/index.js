d32080a04abddfd026ca493fe513846875d37f14
Nick Wittwer <nick.wittwer@me.com>
2018-10-28 16:42:32 +0100

    Migrated to Vue architecture

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-// Required scripts
-import Vue from './required/vue.dev';
-import App from './App'
-
-new Vue({
-  el: 'main',
-  template: '</App>',
-  components: { App }
-});
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import store from './store/index'
 import App from './App.vue'
-import panzoom from "panzoom";
+import panzoom from "panzoom"
 
 // Vue Configuration
 Vue.config.productionTip = false
