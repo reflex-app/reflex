2ca2aa56e84e549362996ff4cf1a8a92df955793
Nick Wittwer <nick.wittwer@me.com>
2018-10-28 21:13:53 +0100

    WIP resize() function

--- test/Artboards.test.js
+++ test/Artboards.test.js
@@ -1,11 +1,13 @@
-import { shallowMount } from '@vue/test-utils'
+import {
+  shallowMount
+} from '@vue/test-utils'
 import Artboards from '../src/components/Artboards'
 
 describe('Artboards', () => {
   const wrapper = shallowMount(Artboards)
   const vm = wrapper.vm
 
-  it('Should be a Vue component', function() {
+  it('Should be a Vue component', function () {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
@@ -23,9 +25,26 @@ describe('Artboards', () => {
     vm.add();
     expect(vm.artboards.length == 2).toBe(true)
   })
-  
+
   it('remove() - Can remove Artboards', () => {
     vm.remove(2)
     expect(vm.artboards.length == 1).toBe(true)
   })
+
+  // it('resize() - Can update localStorage on resize', () => {
+  //   const mock = {
+  //     height: 500,
+  //     width: 500,
+  //     id: 0
+  //   }
+    
+  //   expect(vm.artboards.length == 1).toBe(true)
+  //   vm.resize(mock)
+  //   console.log(vm.artboards[0].width, vm.artboards[0].height);
+
+  //   // Expect the height and width to be updated
+  //   expect(vm.artboards[0].width == 500).toBe(true)
+  //   expect(vm.artboards[0].height == 500).toBe(true)
+  //   expect(vm.artboards[0].id == 1).toBe(true)
+  // })
 })
