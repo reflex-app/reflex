3fa09b0c3f6e92843e35f4d60f2944b45085c9ab
Nick Wittwer <nick.wittwer@me.com>
2018-11-11 11:49:05 +0100

    Removed old panzoom code

--- src/App.vue
+++ src/App.vue
@@ -21,66 +21,6 @@ export default {
     Toolbar
     // TODO: Create Settings
   },
-  methods: {
-    contentWidth() {
-      const width = window.innerWidth / this.$refs.artboards.$el.clientWidth;
-
-      // eslint-disable-next-line
-      console.log(width);
-
-      return Number(width);
-    },
-    contentHeight() {
-      const parentHeight = document.getElementById("canvas-container")
-        .clientHeight;
-
-      let childHeight = this.$refs.artboards.$el.getBoundingClientRect().height;
-
-      const scaleFactor = parentHeight / childHeight;
-
-      // 1. Check if the size is smaller/bigger than the canvas
-      // What if the child doesn't fit inside of the parent? (too big)
-      // Deal with the scale
-      let output;
-      if (childHeight > parentHeight) {
-        // Need to scale down
-        // We'll scale down the childHeight to a size that will fit a screen's width
-        childHeight = parentHeight / 2; // 50% of the parent height
-        output = parentHeight / 2 - childHeight / 2;
-      } else if (childHeight < parentHeight) {
-        // The canvas is too small, scale up
-        // childHeight = childHeight * scaleFactor;
-        output = parentHeight / 2;
-      }
-
-      // Step 2.
-      // const output = (parentHeight / 2) - (childHeight / 2);
-      // const output = parentHeight / 2;
-
-      // (childHeight * (parentHeight  - padding ) ) / parentHeight
-      // AKA the height of the child / the height of the parent
-      // const output = childHeight * scaleFactor; // available space
-
-      // const output = (childHeight * parentHeight) / parentHeight; // available space
-
-      // eslint-disable-next-line
-      console.log({ parentHeight, childHeight, output });
-
-      return Number(output);
-    },
-    minZoom() {
-      return Math.min(
-        this.contentWidth(),
-        document.getElementById("canvas-container").getBoundingClientRect()
-          .height / 2
-      );
-      // return Math.min(this.contentWidth(), this.contentHeight());
-    }
-    // TODO: Add currentZoomScale() to Vuex store
-    // currentZoomScale() {
-    //   return document.panzoomInstance.getTransform();
-    // }
-  },
   mounted: function() {
     let vm = this;
 
@@ -100,20 +40,6 @@ export default {
       });
     });
   }
-  // mounted() {
-
-  //   this.$nextTick(() => {
-  //     // Attach panzoom (pan + zoom-able canvas)
-  //     document.panzoomInstance = panzoom(this.$refs.canvas, {
-  //       maxZoom: 1,
-  //       minZoom: 0.1,
-  //       autoCenter: true
-  //     });
-
-  //     // document.panzoomInstance.zoomAbs(0, 0, this.minZoom());
-  //     document.panzoomInstance.zoomAbs(0, this.contentHeight(), this.minZoom());
-  //   });
-  // }
 };
 </script>
 
