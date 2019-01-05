745d4ab8fb1ff3eba0a5825c2691b2bd6b6985bb
Nick Wittwer <nick.wittwer@me.com>
2019-01-05 13:22:25 +0100

    Added automatic localStorage store

--- src/store/index.js
+++ src/store/index.js
@@ -7,10 +7,20 @@ Vue.use(Vuex)
 // Make Store accessible from components
 export default new Vuex.Store({
   state: {
-    url: "",
-    panzoom: {}
+    url: "", // The current URL being viewed
+    artboards: [], // All artboards on the screen
+    panzoom: {} // Holds Panzoom class info
   },
   mutations: {
+    initLocalStorage(state) {
+			// Check if the ID exists
+			if(localStorage.getItem('store')) {
+				// Replace the state object with the stored item
+				this.replaceState(
+					Object.assign(state, JSON.parse(localStorage.getItem('store')))
+				);
+			}
+    },
     changeURL(state, val) {
       state.url = val; // Update the URL based on the incoming value
 
@@ -19,6 +29,14 @@ export default new Vuex.Store({
     },
     updatePanzoom(state, val) {
       state.panzoom = val;
-    } 
+    },
+    addArtboard(state, artboard) {
+      // console.log(artboard);
+      console.log(artboard);
+      state.artboards.push(artboard);
+    },
+    removeArtboard(state, id) {
+      state.artboards.splice(id, 1);
+    }
   }
 })
