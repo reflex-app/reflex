956ff034862c8de84d3f1b1d132230a8dbb4de37
Nick Wittwer <nick.wittwer@me.com>
2019-01-06 19:54:00 +0100

    Changed position of artboard controls

--- src/components/ArtboardControls.vue
+++ src/components/ArtboardControls.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="ArtboardControls">
     <div id="toolbar__canvas-controls">
-      <div>
-        <span id="canvas-controls__zoomOut" class="button" @click="zoomOut">-</span>
-        <!-- <span id="canvas-controls__scale" v-if="panzoomInstance.transformMatrix">{{ panzoomInstance.transformMatrix[0] }}</span> -->
-        <span id="canvas-controls__zoomIn" class="button" @click="zoomIn">+</span>
-      </div>
+      <span id="canvas-controls__zoomIn" class="button" @click="zoomIn">+</span>
+      <span id="canvas-controls__zoomOut" class="button" @click="zoomOut">-</span>
+      <!-- <span id="canvas-controls__scale" v-if="panzoomInstance.transformMatrix">{{ panzoomInstance.transformMatrix[0] }}</span> -->
       <!-- <a id="canvas-controls__fit-to-screen" @click="center">Center</a> -->
     </div>
     <!-- <div>
       <a id="canvas-controls__orientation" @click="toggleOrientation">Orientation</a>
-    </div> -->
+    </div>-->
   </div>
 </template>
 
@@ -63,7 +61,7 @@ export default {
           label: "Zoom to Fit",
           click: function() {
             vm.center();
-  }
+          }
         })
       );
 
@@ -88,7 +86,6 @@ export default {
 
 #ArtboardControls {
   z-index: 1;
-  // padding: 0.5rem 1rem;
   color: #434343;
   position: relative;
   display: flex;
@@ -97,14 +94,9 @@ export default {
   background: none;
   position: absolute;
   bottom: 1.5rem;
-  right: 1.25rem;
+  left: 1.25rem;
 
-  // box-shadow: 0 6px 10px rgba(#000, 0.3);
-  & > *:not(:first-child) {
-    margin-left: 16px;
-  }
-
-  & > *:nth-child(1) {
+  & > * {
     flex: 1 0 auto;
   }
 
@@ -112,7 +104,7 @@ export default {
     border: none;
     box-sizing: border-box;
     padding: 0.5rem;
-    background: #FFFFFF;
+    background: #ffffff;
     border: 1px solid #979797;
     border-radius: 4px;
     transition: all 0.125s ease-in-out;
@@ -133,10 +125,9 @@ export default {
   #toolbar__canvas-controls {
     user-select: none;
     display: flex;
-    flex-direction: column;
     align-items: center;
 
-    #canvas-controls__zoomOut {
+    & > *:nth-child(1) {
       margin-right: 0.5rem;
     }
 
@@ -147,43 +138,6 @@ export default {
 
   a {
     text-decoration: none;
-  }
-
-  #toolbar__url-container {
-    position: relative;
-
-    #toolbar__recentURLs {
-      display: none;
-      position: absolute;
-      top: calc(100% + 4px);
-      left: 0;
-      width: 100%;
-      background: white;
-      box-shadow: 0 5px 20px rgba(#000, 0.2);
-      transition: all 1s ease-in-out;
-      border-radius: 4px;
-      z-index: 1000;
-
-      &.is-visible {
-        display: block;
-      }
-
-      li {
-        padding: 1rem;
-        list-style: none;
-        color: black;
-
-        &:hover {
-          cursor: pointer;
-          background: rgba($accent-color, 0.1);
-        }
-
-        &.is-keyboard-selected {
-          color: white;
-          background: $accent-color;
-        }
-      }
-    }
   }
 }
 </style>
