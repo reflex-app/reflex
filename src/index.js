be4028fd166adb5924d99abada1e94081b2ad7f0
Nick Wittwer <nick.wittwer@me.com>
2019-01-16 19:17:51 +0100

    Successfully got BrowserSync loading via NW

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // Node Context
+// Not compiled!
+
+// require the module as normal
+const bs = require("browser-sync").create();
+
+// .init starts the server
+bs.init({
+    server: "./app"
+});
+
+// Now call methods on bs instead of the
+// main browserSync module export
+bs.reload("*.html");
