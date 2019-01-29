637e5ad137601287364bf9004a8e3b37b648e8d5
Nick Wittwer <nick.wittwer@me.com>
2019-01-29 20:53:30 +0100

    Added updated homepage template/renderer

---
+++ src/renderer/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "reflex"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./scss/_variables";
+
+#app {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  overscroll-behavior: auto;
+  border-radius: 8px;
+  background: $body-bg;
+}
+</style>
