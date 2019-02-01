00f74f69f75d1d850513862caf1c4318a7fe7a7e
Nick Wittwer <nick.wittwer@me.com>
2019-02-01 22:30:27 +0100

    Added Vue lint fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+    'plugin:vue/base'
+  ],
   globals: {
     __static: true
   },
   plugins: [
-    'html'
+    'vue'
   ],
   'rules': {
     // allow paren-less arrow functions
