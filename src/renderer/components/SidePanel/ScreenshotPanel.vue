8676fa9559254e973fd41b91139e7e3833e6bf06
Nick Wittwer <nick.wittwer@me.com>
2019-02-17 17:35:02 +0100

    Can successfully screenshot and save unique file names

--- src/renderer/components/SidePanel/ScreenshotPanel.vue
+++ src/renderer/components/SidePanel/ScreenshotPanel.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <button @click="capture">Capture</button>
+    <button @click="captureAll()">Capture ALL</button>
+    <div v-for="(artboard, index) in artboards" v-bind:key="index">
+      <span>{{artboard.title}}</span>
+      <button @click="capture(index, artboard.title)">Capture</button>
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,7 @@
 const { dialog } = require("electron").remote;
 const path = require("path");
 const fs = require("fs");
+const moment = require("moment");
 
 export default {
   name: "ScreenshotPanel",
@@ -20,33 +25,50 @@ export default {
   },
 
   methods: {
-    capture() {
-      const webview = document.querySelector("webview");
-      console.log("webview: ", webview);
-      console.log("window:", window);
+    async capture(id, title, screenshotPath) {
+      const webview = document.querySelectorAll("webview")[id];
+      console.log(id, webview);
 
-      function saveScreenshot(screenshot) {
-        // Prompt location to save screenshot
-        dialog.showOpenDialog(
-          { properties: ["openFile", "openDirectory"] },
-          function(filePaths) {
-            try {
-              // Save the screenshot to the selected file path
-              fs.writeFileSync(
-                path.join(filePaths[0], "example.png"),
-                screenshot
-              );
-
-              // Alert the user that the screenshot was saved
-              let notification = new Notification("Screenshot saved", {
-                body: filePaths[0]
-              });
-            } catch (e) {
-              // Nothing selected
-              console.log("No file/directory selected.");
+      async function saveScreenshot(screenshot) {
+        if (!screenshotPath) {
+          // Case: no path set yet (single screenshot save)
+          // Prompt location to save screenshot
+          dialog.showOpenDialog(
+            { properties: ["openFile", "openDirectory"] },
+            function(filePaths) {
+              try {
+                makeFile(filePaths[0], screenshot);
+              } catch (e) {
+                // Nothing was selected
+              }
             }
-          }
-        );
+          );
+        } else {
+          // Case: already has a path (multi-save)
+          makeFile(screenshotPath, screenshot);
+        }
+      }
+
+      // Create the file
+      function makeFile(filePath, screenshot) {
+        const timestamp = moment().format("YYYY-MM-D_h-mm-ssa");
+
+        if (title) {
+          fs.writeFileSync(
+            path.join(filePath, `reflex-screenshot_${title}_${timestamp}.png`),
+            screenshot
+          );
+        } else {
+          fs.writeFileSync(
+            path.join(filePath, `reflex-screenshot_${timestamp}.png`),
+            screenshot
+          );
+        }
+
+        // Alert the user that the screenshot was saved
+        let notification = new Notification("Screenshot saved", {
+          body: filePath
+        });
       }
 
       // Capture the <webview>
@@ -54,6 +76,24 @@ export default {
         const PNG = image.toPNG();
         saveScreenshot(PNG);
       });
+    },
+    async captureAll() {
+      const vm = this;
+
+      // 1. Capture the path to save all
+      dialog.showOpenDialog(
+        { properties: ["openFile", "openDirectory"] },
+        async function(filePaths) {
+          try {
+            // Capture each
+            for (let i = 0; i < vm.artboards.length; i++) {
+              await vm.capture(i, `${i}`, filePaths[0]);
+            }
+          } catch (e) {
+            // Nothing was selected
+          }
+        }
+      );
     }
   }
 };
