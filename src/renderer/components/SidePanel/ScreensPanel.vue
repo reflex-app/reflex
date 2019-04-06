51a55543eb8346b5820d91b7327753b6463e4885
Nick Wittwer <nick.wittwer@me.com>
2019-04-06 12:02:19 +0200

    Componentized the artboards in the sidebar

--- src/renderer/components/SidePanel/ScreensPanel.vue
+++ src/renderer/components/SidePanel/ScreensPanel.vue
@@ -1,66 +1,12 @@
 <template>
   <transition name="sidebar-transition">
     <div id="artboard-tabs">
+      <!-- List of Artboards -->
       <div v-if="artboards.length" class="artboard-tabs__scroll">
-        <draggable v-model="artboards" :options="{draggable:'.artboard-tab'}">
-          <div
-            v-for="artboard in artboards"
-            :key="artboard.id"
-            v-bind="artboard"
-            class="artboard-tab"
-          >
-            <div v-if="editMode==true&&editID==artboard.id" class="editing">
-              <div class="group">
-                <label>Title</label>
-                <input
-                  ref="input"
-                  v-model.lazy="artboard.title"
-                  type="text"
-                  placeholder="Title"
-                  @keyup.enter="save(artboard), editMode=false"
-                >
-              </div>
-              <div class="group">
-                <label>Dimensions</label>
-                <div class="group__input-with-right-label">
-                  <input
-                    v-model.number.lazy="artboard.width"
-                    type="number"
-                    placeholder="Width"
-                    @keyup.enter="save(artboard), editMode=false"
-                  >
-                  <label>W</label>
-                </div>
-                <div class="group__input-with-right-label">
-                  <input
-                    v-model.number.lazy="artboard.height"
-                    type="number"
-                    placeholder="Height"
-                    @keyup.enter="save(artboard), editMode=false"
-                  >
-                  <label>H</label>
-                </div>
-
-                <div class="buttons">
-                  <a href="#" @click="editMode=false">Cancel</a>
-                  <a href="#" @click="save(artboard)">Save</a>
-                </div>
-              </div>
-            </div>
-            <div v-else class="artboard-tab__container">
-              <div class="artboard-tab__container-left">
-                <div>{{ artboard.title }}</div>
-                <div>{{ artboard.width }} x {{ artboard.height }}</div>
-              </div>
-              <div class="artboard-tab__container-right">
-                <a href="#" @click="edit(artboard.id)">Edit</a>
-                <a href="#" @click="remove(artboard.id)">&times;</a>
-              </div>
-            </div>
-          </div>
-        </draggable>
+        <artboardEditable data="artboards"/>
       </div>
 
+      <!-- New Artboard -->
       <div class="artboard-tabs__button button button--primary" @click="add()">New Screen</div>
 
       <!-- Show a tip if there's no artboards -->
@@ -68,6 +14,7 @@
         <div class="empty-state__text">Click to create a new screen</div>
       </div>
 
+      <!-- Templates -->
       <div class="panel-section">
         <span>Add from template:</span>
         <select v-model="defaultSizeSelection" @change="addDefaultSizes">
@@ -82,18 +29,16 @@
 </template>
 
 <script>
-import draggable from "vuedraggable";
+import artboardEditable from "@/components/SidePanel/artboardEditable";
 
 export default {
   name: "ScreensPanel",
   components: {
-    draggable
+    artboardEditable
   },
 
   data() {
     return {
-      editMode: false,
-      editID: null,
       defaultSizeSelection: ""
     };
   },
@@ -205,39 +150,13 @@ export default {
 
       // Empty selected value
       this.defaultSizeSelection = "";
-    },
-    save(artboard) {
-      // Disable editing mode
-      this.editMode = false;
-      this.editID = null;
-
-      // Update the values so that VueJS pays attention
-      this.$store.commit("updateArtboardAtIndex", {
-        id: artboard.id,
-        height: artboard.height || 0,
-        width: artboard.width || 0,
-        title: artboard.title || "Untitled"
-      });
-    },
-    edit(id) {
-      this.editMode = true;
-      this.editID = id;
-
-      // Auto-focus on the first field
-      this.$nextTick(() => {
-        this.$refs.input[0].focus();
-        this.$refs.input[0].select();
-      });
-    },
-    remove(id) {
-      this.$store.commit("removeArtboard", id);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../../scss/_variables";
+@import "~@/scss/_variables";
 
 #artboard-tabs {
   display: flex;
@@ -262,131 +181,6 @@ export default {
     box-sizing: border-box;
     border-radius: 4px;
     width: auto;
-  }
-
-  $artboard-tab-side-padding: 1rem;
-
-  .artboard-tab {
-    flex: 1 0 auto;
-    background: #ffffff;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    line-height: 1.5;
-    border-bottom: 1px solid $border-color;
-
-    &:hover {
-      // background: #f1f1f1;
-    }
-
-    &:active {
-      // background: #e2e2e2;
-    }
-
-    .artboard-tab__container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding: 0.5rem $artboard-tab-side-padding;
-      overflow: auto;
-
-      .artboard-tab__container-left {
-        max-width: 9rem;
-        min-width: 5rem;
-        white-space: nowrap;
-        overflow: hidden;
-        & > * {
-          text-overflow: ellipsis;
-          overflow: hidden;
-        }
-
-        & > *:nth-child(2) {
-          color: gray;
-        }
-      }
-
-      .artboard-tab__container-right {
-        margin-left: 20px;
-
-        // Add space between Edit & Delete links
-        a:not(:last-child) {
-          margin-right: 8px;
-        }
-      }
-    }
-
-    .editing {
-      box-sizing: border-box;
-      padding: 1rem $artboard-tab-side-padding;
-      background: #f4f4f4;
-
-      .buttons {
-        display: flex;
-        justify-content: space-between;
-        margin: 0.5rem 0 0;
-      }
-
-      .group {
-        display: flex;
-        flex-direction: column;
-
-        &:not(:first-child) {
-          margin-top: 1rem;
-        }
-      }
-
-      label {
-        margin-bottom: 0.5rem;
-      }
-
-      .group__input-with-right-label {
-        display: flex;
-        width: 100%;
-        position: relative;
-        align-items: center;
-
-        input {
-          width: 100%;
-        }
-
-        label {
-          position: absolute;
-          top: 0.3rem;
-          right: 1.5rem;
-          color: gray;
-          text-align: right;
-        }
-      }
-
-      input[type="text"],
-      input[type="number"] {
-        position: relative;
-        border: 1px solid $border-color;
-        border-radius: 4px;
-        font-size: 0.9rem;
-        background: white;
-        padding: 0.5rem 0.5rem;
-        outline: none;
-
-        &:not(:last-child) {
-          margin-bottom: 0.25rem;
-        }
-
-        &:hover {
-          // background: white;
-          // box-shadow: 0 0px 0px 1px #cbcbcb;
-        }
-
-        &:focus {
-          border-color: $accent-color;
-          color: $accent-color;
-        }
-
-        & + input {
-          margin-top: 4px;
-        }
-      }
-    }
   }
 
   .empty-state {
---
+++ src/renderer/components/SidePanel/artboardEditable.vue
@@ -0,0 +1,248 @@
+<template>
+  <draggable v-model="artboards" :options="{draggable:'.artboard-tab'}">
+    <div v-for="(artboard, index) in artboards" :key="artboard.id" v-bind="artboard" class="artboard-tab">
+      <!-- Editing state -->
+      <div v-if="editMode==true&&editID==artboard.id" class="editing">
+        <div class="group">
+          <label>Title</label>
+          <input
+            ref="input"
+            v-model.lazy="artboard.title"
+            type="text"
+            placeholder="Title"
+            @keyup.enter="save(artboard), editMode=false"
+          >
+        </div>
+        <div class="group">
+          <label>Dimensions</label>
+          <div class="group__input-with-right-label">
+            <input
+              v-model.number.lazy="artboard.width"
+              type="number"
+              placeholder="Width"
+              @keyup.enter="save(artboard), editMode=false"
+            >
+            <label>W</label>
+          </div>
+          <div class="group__input-with-right-label">
+            <input
+              v-model.number.lazy="artboard.height"
+              type="number"
+              placeholder="Height"
+              @keyup.enter="save(artboard), editMode=false"
+            >
+            <label>H</label>
+          </div>
+
+          <div class="buttons">
+            <a href="#" @click="editMode=false">Cancel</a>
+            <a href="#" @click="save(artboard)">Save</a>
+          </div>
+        </div>
+      </div>
+      <!-- Normal state -->
+      <div v-else class="artboard-tab__container">
+        <div class="artboard-tab__container-left" @click="goToArtboard(index)">
+          <div>{{ artboard.title }}</div>
+          <div>{{ artboard.width }} x {{ artboard.height }}</div>
+        </div>
+        <div class="artboard-tab__container-right">
+          <a href="#" @click="edit(artboard.id)">Edit</a>
+          <a href="#" @click="remove(artboard.id)">&times;</a>
+        </div>
+      </div>
+    </div>
+  </draggable>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "artboardEditable",
+  components: {
+    draggable
+  },
+  props: ["data"],
+  data() {
+    return {
+      editMode: false,
+      editID: null
+    };
+  },
+  computed: {
+    // Bind to our Vuex Store's URL value
+    artboards: {
+      get() {
+        return this.$store.state.artboards;
+      },
+      set(value) {
+        this.$store.commit("setArtboardList", value);
+      }
+    }
+  },
+  methods: {
+    save(artboard) {
+      // Disable editing mode
+      this.editMode = false;
+      this.editID = null;
+
+      // Update the values so that VueJS pays attention
+      this.$store.commit("updateArtboardAtIndex", {
+        id: artboard.id,
+        height: artboard.height || 0,
+        width: artboard.width || 0,
+        title: artboard.title || "Untitled"
+      });
+    },
+    edit(id) {
+      this.editMode = true;
+      this.editID = id;
+
+      // Auto-focus on the first field
+      this.$nextTick(() => {
+        this.$refs.input[0].focus();
+        this.$refs.input[0].select();
+      });
+    },
+    remove(id) {
+      this.$store.commit("removeArtboard", id);
+    },
+    goToArtboard(id) {
+      // Find the artboard (DOM)
+      const artboard = document.getElementsByClassName('artboard')[id]
+      
+      // Move the panzoom container
+       document.$panzoom.scaleToFitEl(artboard)
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+@import "~@/scss/_variables";
+$artboard-tab-side-padding: 1rem;
+
+  .artboard-tab {
+    flex: 1 0 auto;
+    background: #ffffff;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-height: 1.5;
+    border-bottom: 1px solid $border-color;
+
+    &:hover {
+      // background: #f1f1f1;
+    }
+
+    &:active {
+      // background: #e2e2e2;
+    }
+
+    .artboard-tab__container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.5rem $artboard-tab-side-padding;
+      overflow: auto;
+
+      .artboard-tab__container-left {
+        max-width: 9rem;
+        min-width: 5rem;
+        white-space: nowrap;
+        overflow: hidden;
+        & > * {
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+
+        & > *:nth-child(2) {
+          color: gray;
+        }
+      }
+
+      .artboard-tab__container-right {
+        margin-left: 20px;
+
+        // Add space between Edit & Delete links
+        a:not(:last-child) {
+          margin-right: 8px;
+        }
+      }
+    }
+
+    .editing {
+      box-sizing: border-box;
+      padding: 1rem $artboard-tab-side-padding;
+      background: #f4f4f4;
+
+      .buttons {
+        display: flex;
+        justify-content: space-between;
+        margin: 0.5rem 0 0;
+      }
+
+      .group {
+        display: flex;
+        flex-direction: column;
+
+        &:not(:first-child) {
+          margin-top: 1rem;
+        }
+      }
+
+      label {
+        margin-bottom: 0.5rem;
+      }
+
+      .group__input-with-right-label {
+        display: flex;
+        width: 100%;
+        position: relative;
+        align-items: center;
+
+        input {
+          width: 100%;
+        }
+
+        label {
+          position: absolute;
+          top: 0.3rem;
+          right: 1.5rem;
+          color: gray;
+          text-align: right;
+        }
+      }
+
+      input[type="text"],
+      input[type="number"] {
+        position: relative;
+        border: 1px solid $border-color;
+        border-radius: 4px;
+        font-size: 0.9rem;
+        background: white;
+        padding: 0.5rem 0.5rem;
+        outline: none;
+
+        &:not(:last-child) {
+          margin-bottom: 0.25rem;
+        }
+
+        &:hover {
+          // background: white;
+          // box-shadow: 0 0px 0px 1px #cbcbcb;
+        }
+
+        &:focus {
+          border-color: $accent-color;
+          color: $accent-color;
+        }
+
+        & + input {
+          margin-top: 4px;
+        }
+      }
+    }
+  }
+</style>
