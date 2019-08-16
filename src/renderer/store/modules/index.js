fb00148375c51749aca54d761c4034962a6c2953
Nick Wittwer <nick.wittwer@me.com>
2019-08-16 23:28:10 +0200

    Added fix for require.context in Jest tests

--- src/renderer/store/modules/index.js
+++ src/renderer/store/modules/index.js
@@ -3,6 +3,41 @@
  * in a one-shot manner. There should not be any reason to edit this file.
  */
 
+if (typeof require.context === 'undefined') {
+  const fs = require('fs')
+  const path = require('path')
+
+  require.context = (base = '.', scanSubDirectories = false, regularExpression = /\.js$/) => {
+    const files = {}
+
+    function readDirectory(directory) {
+      fs.readdirSync(directory).forEach((file) => {
+        const fullPath = path.resolve(directory, file)
+
+        if (fs.statSync(fullPath).isDirectory()) {
+          if (scanSubDirectories) readDirectory(fullPath)
+
+          return
+        }
+
+        if (!regularExpression.test(fullPath)) return
+
+        files[fullPath] = true
+      })
+    }
+
+    readDirectory(path.resolve(__dirname, base))
+
+    function Module(file) {
+      return require(file)
+    }
+
+    Module.keys = () => Object.keys(files)
+
+    return Module
+  }
+}
+
 const files = require.context('.', false, /\.js$/)
 const modules = {}
 
