bdffb0f838531991b0661245fa932276ad0ce345
Nick Wittwer <nick.wittwer@me.com>
2019-08-17 00:51:44 +0200

    Removed old localStorage module

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -8,18 +8,8 @@ Vue.use(Vuex)
 
 // Create the Store
 const store = new Vuex.Store({
-  // namespaced: true,
   modules,
   strict: process.env.NODE_ENV !== 'production'
-})
-
-// Initialize localStorage
-store.dispatch('initLocalStorage')
-
-// Subscribe to store updates
-store.subscribe((mutation, state) => {
-  // Store the state object as a JSON string
-  localStorage.setItem('store', JSON.stringify(state))
 })
 
 export default store
