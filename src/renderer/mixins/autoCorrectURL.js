9f1d2183e55c14fe0f67420cd0ca1b85b87c4185
Nick Wittwer <nick.wittwer@me.com>
2019-08-17 13:22:50 +0200

    Added test for autoCorrectURL

--- src/renderer/mixins/autoCorrectURL.js
+++ src/renderer/mixins/autoCorrectURL.js
@@ -12,36 +12,29 @@ export default function (url) {
     let hasLocalhost = false
     let hasLocalPath = false
 
+    // Convert URL to lowercase
+    url = url.toLowerCase()
+
     // Step 1: Does it have http:// or https:// ?
     // The "?" in the Regex accepts http or https
-    if (new RegExp(/^https?/).test(url) == true) {
+    if (new RegExp(/^https?/).test(url) === true) {
       hasHttpPrefix = true
-    } else if (new RegExp(/^HTTPS?/).test(url)) {
-      // Case: HTTP://somesite.com/SOMETHING
-      //         ^ only replace this part
-      if (new RegExp(/^HTTP/).test(url)) {
-        url = url.replace(/^HTTP/, 'http')
-        hasHttpPrefix = true
-      } else if (new RegExp(/^HTTPS/).test(url)) {
-        url = url.replace(/^HTTPS/, 'https')
-        hasHttpPrefix = true
-      }
     }
 
     // Step 2: Does it have .* (i.e. ".com") ?
-    if (url.includes('.') == true) {
+    if (url.includes('.') === true) {
       hasDot = true
     }
 
     // Step 3: Does it include "localhost"?
-    if (url.includes('localhost') == true) {
+    if (url.includes('localhost') === true) {
       hasLocalhost = true
     }
 
     // Step 4: Does it include:
     // Mac: "file://"
     // Windows: "file:///C:/"
-    if (url.includes('file://') == true) {
+    if (url.includes('file://') === true) {
       hasLocalPath = true
     }
 
@@ -67,13 +60,14 @@ export default function (url) {
       // Perfect format:
       // http[s]://example.com
       resolve(url)
-    } else if (hasHttpPrefix == false && hasDot == true) {
+    } else if (hasHttpPrefix === false && hasDot === true) {
       // Case: example.com
       // Check if URL starts with anything besides a letter or digit
-      if (new RegExp(/^[0-9a-z]/).test(url) == false) {
+      if (new RegExp(/^[0-9a-z]/).test(url) === false) {
         reject()
       } else {
-        // The URL can be prepended by http://
+        // TODO: Default should be http
+        // The URL can be prepended by https://
         url = 'https://' + url
         resolve(url)
       }
