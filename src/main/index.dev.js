82f89eb48024530e444a9e118a2011169e7ee043
Nick Wittwer <nick.wittwer@me.com>
2019-08-25 09:08:54 +0200

    Updated file to ES6 imports

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -3,21 +3,21 @@
  * `electron-debug` & `vue-devtools`. There shouldn't be any need to
  *  modify this file, but it can be used to extend your development
  *  environment.
+ *
+ *  Updated based on: https://github.com/SimulatedGREG/electron-vue/issues/389#issuecomment-446165474
  */
 
-/* eslint-disable */
+const { app } = require('electron')
+const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer')
 
 // Install `electron-debug` with `devtron`
-require('electron-debug')({ showDevTools: true })
+require('electron-debug')()
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
-    .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
-    })
+app.on('ready', () => {
+  installExtension(VUEJS_DEVTOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('Unable to install `vue-devtools`: ', err))
 })
 
 // Require `main` process to boot app
