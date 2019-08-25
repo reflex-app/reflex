b197de92635ae63e526f41865c1bbadb9cd984c9
Nick Wittwer <nick.wittwer@me.com>
2019-08-25 23:15:21 +0200

    Improved usage of Vuex-Persist

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -6,13 +6,8 @@ import modules from './modules'
 
 Vue.use(Vuex)
 
-// Persisted State
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage
-})
-
 export default new Vuex.Store({
   modules,
-  plugins: [vuexLocal.plugin],
-  strict: process.env.NODE_ENV !== 'production'
+  strict: process.env.NODE_ENV !== 'production',
+  plugins: [new VuexPersistence().plugin]
 })
