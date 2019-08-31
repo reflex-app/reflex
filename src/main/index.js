95e222a052f076a92255d6153668785a8d4fa514
Nick Wittwer <nick.wittwer@me.com>
2019-08-31 18:52:27 +0200

    Fixed bug causing build to not launch

--- src/main/index.js
+++ src/main/index.js
@@ -23,7 +23,7 @@ app.getVersion = () => version
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (!isDev) {
+if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
