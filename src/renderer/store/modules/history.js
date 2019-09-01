6429882f0b9d892928d1b8b2c948f6f057476627
Nick Wittwer <nick.wittwer@me.com>
2019-09-01 12:10:47 +0200

    Single quotes

--- src/renderer/store/modules/history.js
+++ src/renderer/store/modules/history.js
@@ -17,23 +17,23 @@ const mutations = {
   },
   setCurrentPageIndex: (state, payload) => {
     switch (payload) {
-      case "forward":
+      case 'forward':
         state.currentPage.index = state.currentPage.index + 1
-        break;
+        break
 
-      case "back":
+      case 'back':
         state.currentPage.index = state.currentPage.index - 1
-        break;
+        break
 
         // If adding a new page (i.e. user has entered a URL in the search input bar)
-      case "new":
+      case 'new':
         state.currentPage.index = state.pages.length - 1 // zero indexed
-        break;
+        break
 
         // Allow to set to a specific index
       default:
         state.currentPage.index = payload
-        break;
+        break
     }
   },
   /** Sets the Site Title (via iFrame)
