5f61fd20e1d546b7f99c14369095b5b3669f3cf9
Nick Wittwer <hi@nickwittwer.com>
2020-01-24 15:39:58 +0100

    Added Gulp files

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp')
+const HubRegistry = require('gulp-hub')
+
+// Configure tasks
+const hub = new HubRegistry([
+  './gulp-tasks/tasks/**/*.js'
+])
+
+// Load tasks
+gulp.registry(hub)
