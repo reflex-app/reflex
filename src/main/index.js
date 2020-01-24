501dfccfe8fa003cfe3d6b9926fc5f44cca092db
Nick Wittwer <hi@nickwittwer.com>
2020-01-24 15:39:44 +0100

    Added main scripts for Electron

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,14 @@
 /* globals INCLUDE_RESOURCES_PATH */
-import { app } from 'electron'
+import {
+  app
+} from 'electron'
+
+import {
+  version
+} from '../../package.json'
+
+// Set the version
+app.getVersion = () => version
 
 /**
  * Set `__resources` path to resources files in renderer process
@@ -8,6 +17,36 @@ global.__resources = undefined // eslint-disable-line no-underscore-dangle
 // noinspection BadExpressionStatementJS
 INCLUDE_RESOURCES_PATH // eslint-disable-line no-unused-expressions
 if (__resources === undefined) console.error('[Main-process]: Resources path is undefined')
+
+/**
+ * Webview Settings
+ */
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-attach-webview', (event, webPreferences, params) => {
+    webPreferences.nodeIntegration = false // Disable Node.js integration inside <webview>
+    // webPreferences.webSecurity = false // Disable web security
+
+    // Strip away preload scripts if unused or verify their location is legitimate
+    delete webPreferences.preload
+    delete webPreferences.preloadURL
+  })
+})
+
+/** 
+ * Workarounds for accessing HTTP/HTTPS sites with Browsersync & Chromium
+ */
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+app.commandLine.appendSwitch('allow-insecure-localhost', 'true');
+
+// app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+//   // On certificate error we disable default behaviour (stop loading the page)
+//   // and we then say "it is all fine - true" to the callback
+//   event.preventDefault()
+//   callback(true)
+// }).on('select-client-certificate', (event, webContents, url, list, callback) => {
+//   event.preventDefault()
+//   callback(list[0])
+// })
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
---
+++ src/main/menu.js
@@ -0,0 +1,202 @@
+const {
+  app,
+  Menu,
+  shell
+} = require('electron')
+const isDev = require('electron-is-dev')
+
+export function setMenu(window) {
+  const template = [{
+    label: 'Edit',
+    submenu: [{
+      role: 'undo'
+    },
+    {
+      role: 'redo'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      role: 'cut'
+    },
+    {
+      role: 'copy'
+    },
+    {
+      role: 'paste'
+    },
+    {
+      role: 'pasteandmatchstyle'
+    },
+    {
+      role: 'delete'
+    },
+    {
+      role: 'selectall'
+    }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      // {
+      //   role: 'resetzoom'
+      // },
+      {
+        label: 'Zoom In',
+        accelerator: 'CmdOrCtrl+=',
+        // role: 'zoomin',
+        click() {
+          window.webContents.send('menu_zoom-in')
+        }
+      },
+      {
+        label: 'Zoom Out',
+        accelerator: 'CmdOrCtrl+-',
+        // role: 'zoomout'
+        click() {
+          window.webContents.send('menu_zoom-out')
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      },
+      {
+        label: 'Center to Screen',
+        accelerator: 'CmdOrCtrl+0',
+        click() {
+          window.webContents.send('menu_zoom-to-fit')
+        }
+      },
+      {
+        type: 'separator'
+      },
+      // {
+      //   role: 'reload'
+      // },
+      {
+        role: 'forcereload'
+      },
+      {
+        role: 'toggledevtools'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [{
+      role: 'minimize'
+    },
+    {
+      role: 'close'
+    }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      //   {
+      //   label: 'Community',
+      //   click() {
+      //     shell.openExternal('https://spectrum.chat/reflex-app')
+      //   }
+      // },
+      {
+        label: 'Report a Bug',
+        click() {
+          shell.openExternal('https://github.com/nwittwer/reflex/issues/new')
+        }
+      },
+      {
+        label: 'Follow on Twitter',
+        click() {
+          shell.openExternal('https://twitter.com/reflex_app')
+        }
+      }
+    ]
+  }
+  ]
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.name,
+      submenu: [{
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'services'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+      ]
+    })
+  }
+
+  // Edit menu
+  template[1].submenu.push({
+    type: 'separator'
+  }, {
+    label: 'Speech',
+    submenu: [{
+      role: 'startspeaking'
+    },
+    {
+      role: 'stopspeaking'
+    }
+    ]
+  })
+
+  // Window menu
+  template[3].submenu = [{
+    role: 'minimize'
+  },
+  {
+    role: 'zoom'
+  },
+  {
+    type: 'separator'
+  },
+  {
+    role: 'front'
+  }
+  ]
+
+  // If in Dev mode, add menu
+  if (isDev) {
+    template.splice(4, 0, {
+      label: 'Developer',
+      submenu: [{
+        label: 'Show Canvas Debugger',
+        click() {
+          window.webContents.send('menu_show-developer-canvas-debugger')
+        }
+      }]
+    })
+  }
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
