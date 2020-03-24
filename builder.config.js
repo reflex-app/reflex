f938dea19dbf4ec08f03dff4e50d59129c14249a
Nick Wittwer <nick.wittwer@me.com>
2020-03-24 20:01:51 +0100

    WIP refactoring

--- builder.config.js
+++ builder.config.js
@@ -1,25 +1,6 @@
 const isRelease = process.env.RELEASE == 'true'
 const ICONS_DIR = 'build/icons/'
 
-const windowsOS = {
-  win: {
-    icon: ICONS_DIR + 'icon.ico',
-    publisherName: 'Nick Wittwer',
-    target: 'nsis',
-    publish: ['github'] // Automatically publish artifacts to Github
-  },
-  nsis: {
-    differentialPackage: true
-  }
-}
-
-const linuxOS = {
-  linux: {
-    icon: ICONS_DIR,
-    target: 'deb'
-  }
-}
-
 const macOS = {
   mac: {
     target: 'dmg',
@@ -28,7 +9,7 @@ const macOS = {
     gatekeeperAssess: false, // Required for MacOS Catalina
     entitlements: "build/entitlements.mac.plist", // Required for MacOS Catalina
     entitlementsInherit: "build/entitlements.mac.plist", // Required for MacOS Catalina
-    publish: isRelease ? ['github'] : null //  Publish artifacts to Github (only for deploys)
+    publish: isRelease ? ['github'] : null //  Publish artifacts to Github (release)
   },
   dmg: {
     sign: false, // Required for MacOS Catalina
@@ -44,6 +25,25 @@ const macOS = {
         type: 'file'
       }
     ]
+  }
+}
+
+const windowsOS = {
+  win: {
+    icon: ICONS_DIR + 'icon.ico',
+    publisherName: 'Nick Wittwer',
+    target: 'nsis',
+    publish: isRelease ? ['github'] : null // Publish artifacts to Github (release)
+  },
+  nsis: {
+    differentialPackage: true
+  }
+}
+
+const linuxOS = {
+  linux: {
+    icon: ICONS_DIR,
+    target: 'deb'
   }
 }
 
