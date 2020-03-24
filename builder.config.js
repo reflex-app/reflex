cd7974d021ecf247969fdc12e3ee36441588ff27
Nick Wittwer <nick.wittwer@me.com>
2020-03-24 13:16:55 +0100

    Added notarization to builder config and added script for afterSign

--- builder.config.js
+++ builder.config.js
@@ -1,10 +1,12 @@
+const isRelease = process.env.RELEASE == 'true'
 const ICONS_DIR = 'build/icons/'
 
 const windowsOS = {
   win: {
     icon: ICONS_DIR + 'icon.ico',
     publisherName: 'Nick Wittwer',
-    target: 'nsis'
+    target: 'nsis',
+    publish: ['github'] // Automatically publish artifacts to Github
   },
   nsis: {
     differentialPackage: true
@@ -22,9 +24,14 @@ const macOS = {
   mac: {
     target: 'dmg',
     icon: ICONS_DIR + 'icon.icns',
-    publish: ['github']
+    hardenedRuntime: true, // Required for MacOS Catalina
+    gatekeeperAssess: false, // Required for MacOS Catalina
+    entitlements: "build/entitlements.mac.plist", // Required for MacOS Catalina
+    entitlementsInherit: "build/entitlements.mac.plist", // Required for MacOS Catalina
+    publish: isRelease ? ['github'] : null //  Publish artifacts to Github (only for deploys)
   },
   dmg: {
+    sign: false, // Required for MacOS Catalina
     contents: [{
         x: 410,
         y: 150,
@@ -60,6 +67,7 @@ module.exports = {
       to: 'dist/renderer/'
     }
   ],
+  afterSign: isRelease ? "scripts/notarize.js" : null, // Notarize Mac (ONLY for deploys)
   ...windowsOS,
   ...linuxOS,
   ...macOS
