836b9ca31e545d89ea0373a72c13368f3f1420de
Nick Wittwer <nick.wittwer@me.com>
2020-03-24 23:01:22 +0100

    Don't codesign win

--- builder.config.js
+++ builder.config.js
@@ -34,6 +34,7 @@ const windowsOS = {
     icon: ICONS_DIR + 'icon.ico',
     publisherName: 'Nick Wittwer',
     target: 'nsis',
+    verifyUpdateCodeSignature: false, // Don't codesign https://github.com/electron-userland/electron-builder/issues/2786#issuecomment-383813995
     publish: isRelease ? ['github'] : null // Publish artifacts to Github (release)
   },
   nsis: {
