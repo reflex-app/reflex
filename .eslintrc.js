e7bce096c6c348bf99d50e2f4ab4ee3b0f430203
Nick Wittwer <nick.wittwer@me.com>
2020-04-17 20:42:47 +0200

    Added eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,63 @@
+// module.exports = {
+//   root: true,
+//   parserOptions: {
+//     parser: 'babel-eslint',
+//     sourceType: 'module'
+//   },
+//   env: {
+//     browser: true,
+//     node: true,
+//     jest: true
+//   },
+//   extends: [
+//     'standard',
+//     'plugin:vue/recommended',
+//     'plugin:vue/base'
+//   ],
+//   globals: {
+//     __static: true
+//   },
+//   plugins: [
+//     'vue'
+//   ],
+//   'rules': {
+//     // allow paren-less arrow functions
+//     'arrow-parens': 0,
+//     // stop being annoying about spaces before parameters
+//     'space-before-function-paren': 0,
+//     // allow async-await
+//     'generator-star-spacing': 0,
+//     // allow new()
+//     'no-new': 0,
+//     // allow debugger during development
+//     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+//   }
+// }
+
+
+
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
-  },
   env: {
     browser: true,
-    node: true,
-    jest: true
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   },
   extends: [
-    'standard',
-    'plugin:vue/recommended',
-    'plugin:vue/base'
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
   ],
-  globals: {
-    __static: true
-  },
   plugins: [
-    'vue'
+    'prettier'
   ],
-  'rules': {
+  // add your custom rules here
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // stop being annoying about spaces before parameters
