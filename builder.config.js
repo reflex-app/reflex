62a84b5f9b68aa4a429b9bb4435aa6023018738b
Nick Wittwer <nick.wittwer@me.com>
2020-12-14 16:56:46 +0100

    Attempted fix for asar build issue

--- builder.config.js
+++ builder.config.js
@@ -9,7 +9,7 @@ const macOS = {
     gatekeeperAssess: false, // Required for MacOS Catalina
     entitlements: 'build/entitlements.mac.plist', // Required for MacOS Catalina
     entitlementsInherit: 'build/entitlements.mac.plist', // Required for MacOS Catalina
-    publish: isRelease ? ['github'] : null, //  Publish artifacts to Github (release)
+    publish: isRelease ? ['github'] : null //  Publish artifacts to Github (release)
   },
   afterSign: isRelease ? 'scripts/notarize.js' : null, // Notarize Mac (ONLY for deploys)
   dmg: {
@@ -19,15 +19,15 @@ const macOS = {
         x: 410,
         y: 150,
         type: 'link',
-        path: '/Applications',
+        path: '/Applications'
       },
       {
         x: 130,
         y: 150,
-        type: 'file',
-      },
-    ],
-  },
+        type: 'file'
+      }
+    ]
+  }
 }
 
 const windowsOS = {
@@ -36,34 +36,34 @@ const windowsOS = {
     // publisherName: 'Nick Wittwer',
     // target: 'nsis',
     // verifyUpdateCodeSignature: false, // Don't codesign https://github.com/electron-userland/electron-builder/issues/2786#issuecomment-383813995
-    publish: isRelease ? ['github'] : null, // Publish artifacts to Github (release)
-  },
+    publish: isRelease ? ['github'] : null // Publish artifacts to Github (release)
+  }
   // nsis: {
   //   differentialPackage: true
   // }
 }
 
 module.exports = {
-  asar: true,
+  asar: false, // Should be false
   productName: require('./package.json').productName,
   appId: 'com.reflex.app',
   // eslint-disable-next-line no-template-curly-in-string
   artifactName: 'Reflex-${version}.${ext}',
   directories: {
-    output: 'build',
+    output: 'build'
   },
   // default files: https://www.electron.build/configuration/contents
   files: [
     'package.json',
     {
       from: 'dist/main/',
-      to: 'dist/main/',
+      to: 'dist/main/'
     },
     {
       from: 'dist/renderer',
-      to: 'dist/renderer/',
-    },
+      to: 'dist/renderer/'
+    }
   ],
   ...macOS,
-  ...windowsOS,
+  ...windowsOS
 }
