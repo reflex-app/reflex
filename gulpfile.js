04eb016e52828edde307cbfbb195e24228162f0e
Nick Wittwer <nick.wittwer@me.com>
2020-12-18 11:22:03 +0100

    Removed Gulp

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const HubRegistry = require('gulp-hub')
-
-// Configure tasks
-const hub = new HubRegistry(['./gulp-tasks/tasks/**/*.js'])
-
-// Load tasks
-gulp.registry(hub)
