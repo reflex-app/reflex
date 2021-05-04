2c21eee8419d53f46752177e0f039f1e67ea3737
Nick Wittwer <nick.wittwer@me.com>
2021-05-04 22:47:13 +0200

    Added better Git tag handling and confirmation about pushing to origin

--- scripts/updater-cli.js
+++ scripts/updater-cli.js
@@ -19,8 +19,8 @@ function updateApp() {
 
   inquirer
     .prompt({
-      type: 'list',
       name: 'updateType',
+      type: 'list',
       message: 'What kind of update is this?',
       choices: ['major', 'minor', 'patch', 'prerelease'],
       default: 'prerelease',
@@ -30,19 +30,57 @@ function updateApp() {
       shell.cd(pathToApp)
 
       // Bump version
-      console.log(`Updating ${answers.updateType}...`)
+      console.log(`Bumping ${answers.updateType}...`)
       shell.exec(`yarn version ${answers.updateType}`, { silent: true })
 
       // Get updated version
-      const newVersion = requireUncached(pkg).version
+      const newVersion = 'v' + requireUncached(pkg).version
 
       // Git Commit
       console.log(`Committing`)
-      shell.exec(`git commit -am v${newVersion}`)
+      shell.exec(`git add package.json && git commit -m ${newVersion}`)
 
+      ////////////////////////////////
       // Git Tag
+
       console.log(`Tagging`)
-      shell.exec(`git tag v${newVersion}`)
+
+      // Pull from latest tags
+      shell.exec('git fetch --tags --all --prune')
+
+      // Delete existing tag
+      const deleteExistingTag = () => {
+        const { stdout: tagExists } = shell.exec(`git tag`)
+        if (tagExists) {
+          shell.exec(`git tag -d ${newVersion}`)
+        }
+      }
+      deleteExistingTag()
+
+      // Create Git tag
+      shell.exec(`git tag ${newVersion}`)
+
+      ////////////////////////////////
+
+      // Are you sure?
+      inquirer
+        .prompt({
+          name: 'confirmUpdate',
+          type: 'confirm',
+          message: `Do you want to Git push ${newVersion} to origin? This will also push any other other local commits, and a release build will start.`,
+          default: false,
+        })
+        .then(({ confirmUpdate }) => {
+          if (confirmUpdate === true) {
+            // Push to remote
+            console.log(`Pushing to remote`)
+            shell.exec(`git push && git push origin ${newVersion}`) // Push the commit & tag
+          } else {
+            console.log(
+              'The tag and commits are done. You may push when ready.'
+            )
+          }
+        })
 
       // Return too root
       shell.cd(initialPath)
