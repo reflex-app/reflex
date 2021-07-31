1d29562b2c8dca419918404e697136647940f1ba
Nick Wittwer <nick.wittwer@me.com>
2021-07-31 22:26:50 +0200

    Updated UI package's Storybook and moved to Vite instead of Webpack

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,17 +1,21 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
-    builder: 'webpack5',
+    builder: 'storybook-builder-vite',
+  },
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+
+    config.resolve.alias['~reflex/ui'] = path.resolve(
+      __dirname,
+      '../src/components'
+    )
+
+    // return the customized config
+    return config
   },
 }
--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -1,71 +1,27 @@
 <template>
-  <button class="it-btn" :class="rootClasses" :disabled="disabled">
-    <it-icon v-if="icon" class="it-btn-icon" :name="icon" />
-    <span v-if="$slots.default" class="it-btn-text">
-      <slot />
-    </span>
-    <span class="it-btn-wrap-loading">
-      <it-loading v-if="loading" color="#fff" :radius="10" :stroke="3" />
-    </span>
-  </button>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue'
-// import ItIcon from '../icon'
-// import ItLoading from '../loading'
-// import { Sizes, Colors } from '@/models/enums'
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
 export default defineComponent({
-  name: 'it-button',
-  // components: {
-  //   ItIcon,
-  //   ItLoading,
-  // },
-  props: {
-    type: {
-      type: String,
-      // default: Colors.NEUTRAL,
-      validator: (value: Colors) => Object.values(Colors).includes(value),
-    },
-    size: {
-      type: String,
-      // default: Sizes.NORMAL,
-      validator: (value: Sizes) => Object.values(Sizes).includes(value),
-    },
-    iconAfter: { type: Boolean },
-    disabled: { type: Boolean },
-    outlined: { type: Boolean },
-    round: { type: Boolean },
-    pulse: { type: Boolean },
-    loading: { type: Boolean },
-    block: { type: Boolean },
-    text: { type: Boolean },
-    icon: { type: String, default: null },
-  },
-  setup(props, { slots }) {
-    const rootClasses = computed(() => [
-      {
-        pulse: props.pulse,
-        'it-btn--empty': !slots.default,
-        'it-btn--outlined': props.outlined,
-        'it-btn--round': props.round,
-        'it-btn--block': props.block,
-        'it-btn--text': props.text,
-        'it-btn--loading': props.loading,
-        [`it-btn--${props.size}`]: props.size,
-        ...(props.type
-          ? { [`it-btn--${props.type}`]: true }
-          : { 'it-btn--neutral': true }),
-        ...(props.icon
-          ? {
-              [props.iconAfter
-                ? 'it-btn--icon-right'
-                : 'it-btn--icon-left']: true,
-            }
-          : null),
-      },
-    ])
-    return { rootClasses }
-  },
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 })
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
