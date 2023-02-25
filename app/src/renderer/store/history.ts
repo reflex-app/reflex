b73f091711475e51c6f58f052f8b858b39378d7c
Nick Wittwer <6360459+nwittwer@users.noreply.github.com>
2023-02-25 10:35:13 +0100

    Fixed double "this.this" reference in store/history

--- app/src/renderer/store/history.ts
+++ app/src/renderer/store/history.ts
@@ -1,7 +1,17 @@
 import { defineStore } from 'pinia'
 
+interface State {
+  pages: any[]
+  currentPage: {
+    index: number
+    url: string
+    title: string | null
+    favicon: string | null
+  }
+}
+
 export const useHistoryStore = defineStore('history', {
-  state: () => ({
+  state: (): State => ({
     pages: [],
     currentPage: {
       index: 0,
@@ -11,36 +21,39 @@ export const useHistoryStore = defineStore('history', {
     },
   }),
   actions: {
-    updateHistory: (payload) => {
-      Object.assign(this.pages, payload)
-    },
-    addPage: (payload) => {
-      this.pages.push(payload)
-    },
     back() {
       this.setCurrentPageIndex('back')
     },
     forward() {
       this.setCurrentPageIndex('forward')
     },
-    setCurrentPageIndex: (payload) => {
+    reload() {
+      // TODO: Trigger reload!
+    },
+    updateHistory(payload) {
+      Object.assign(this.pages, payload)
+    },
+    addPage(payload) {
+      this.pages.push(payload)
+    },
+    setCurrentPageIndex(payload) {
       switch (payload) {
         case 'forward':
-          this.this.currentPage.index = this.this.currentPage.index + 1
+          this.currentPage.index = this.currentPage.index + 1
           break
 
         case 'back':
-          this.this.currentPage.index = this.this.currentPage.index - 1
+          this.currentPage.index = this.currentPage.index - 1
           break
 
         // If adding a new page (i.e. user has entered a URL in the search input bar)
         case 'new':
-          this.this.currentPage.index = this.pages.length - 1 // zero indexed
+          this.currentPage.index = this.pages.length - 1 // zero indexed
           break
 
         // Allow to set to a specific index
         default:
-          this.this.currentPage.index = payload
+          this.currentPage.index = payload
           break
       }
     },
@@ -48,8 +61,10 @@ export const useHistoryStore = defineStore('history', {
      * @param  {} state
      * @param  {} val
      */
-    changeSiteData: (val) => {
+    changeSiteData(val: { title?: string; url?: string; favicon?: string }) {
       if (!val) throw new Error('No value')
+      // This function may be called just to trigger a new URL navigation
+      // but it can also be called from an <artboard> once it has access to the new URL
 
       // @TODO: Throttle this fn, as it will be called by
       // each <webview> when it loads, and doesn't need to keep changing
@@ -69,9 +84,6 @@ export const useHistoryStore = defineStore('history', {
     addPageToHistory(payload) {
       this.addPage(payload) // Add to the array
       this.setCurrentPageIndex('new') // Update the current page
-    },
-    reload() {
-      // TODO: Trigger reload!
     },
   },
 })
