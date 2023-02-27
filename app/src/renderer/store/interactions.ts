74bec9c60f42c7eeca51b6744f3c283d94b46785
Nick Wittwer <6360459+nwittwer@users.noreply.github.com>
2023-02-27 07:35:25 +0100

    Reenabled isSelectingArea and removed arrow functions in store getters

--- app/src/renderer/store/interactions.ts
+++ app/src/renderer/store/interactions.ts
@@ -1,9 +1,11 @@
 import { defineStore } from 'pinia'
+import { watch } from 'vue'
 
 interface State {
   internal: {
     isPanzooming: boolean
     isResizingArtboard: boolean
+    isSelectingArea: boolean
   }
   isWebInteractionContext: boolean
   panzoomEnabled: boolean
@@ -15,18 +17,16 @@ export const useInteractionStore = defineStore('interactions', {
     // isZooming: false,
     internal: {
       isPanzooming: false,
-      // isSelectingArea: false,
+      isSelectingArea: false,
       isResizingArtboard: false,
     },
     isWebInteractionContext: false,
     panzoomEnabled: true,
   }),
   getters: {
-    isInteracting: (state) => {
-      /**
-       * Getter to check if any states are true
-       * Is Interacting = true when any internal state is true
-       */
+    isInteracting(state) {
+      // Check if any of the internal properties are set to true
+      // If so, we say the user is interacting
       let isOn = false
 
       for (const s in state.internal) {
@@ -42,7 +42,7 @@ export const useInteractionStore = defineStore('interactions', {
      * Is the current user in the
      * app context or the web interaction context?
      */
-    currentContext: (state) => {
+    currentContext(state) {
       // TODO this should also take into consideration if 1+ screens are selected,
       // or if there is a panzoom event happening
       return state.isWebInteractionContext ? 'web' : 'app'
@@ -54,7 +54,7 @@ export const useInteractionStore = defineStore('interactions', {
         this.internal[key] = value
       }
     },
-    setPanzoomState({ value }) {
+    setPanzoomState(value) {
       this.panzoomEnabled = value
     },
     setWebInteractionState(value) {
