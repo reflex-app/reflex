fc1cf6d9177c22928d51f4be9f483a38feb6189c
Nick Wittwer <6360459+nwittwer@users.noreply.github.com>
2023-02-27 07:32:15 +0100

    Refactored Panzoom

--- app/src/renderer/components/panzoom/Panzoom.vue
+++ app/src/renderer/components/panzoom/Panzoom.vue
@@ -1,19 +1,23 @@
 <template>
-  <div
-    class="panzoom-container"
-    :class="{
-      'dev-visual-debugger': showCanvasDebugger,
-    }"
-  >
-    <PanzoomControls v-if="panzoomInstance" :instance="panzoomInstance" />
+  <div style="display: inline-block">
+    <!-- We 'panzoom-exclude' to mark this as not relevant to Panzoom -->
+    <!-- <PanzoomControls v-if="panzoomInstance" :instance="panzoomInstance" /> -->
     <div
-      ref="parent"
       id="canvas"
+      ref="outerPanArea"
+      class="panzoom-container"
       :class="{
         'dev-visual-debugger': showCanvasDebugger,
       }"
     >
-      <slot />
+      <div
+        ref="innerPanArea"
+        :class="{
+          'dev-visual-debugger': showCanvasDebugger,
+        }"
+      >
+        <slot />
+      </div>
     </div>
   </div>
 </template>
@@ -40,8 +44,8 @@ const panzoomEnabled = computed(() => interactions.panzoomEnabled)
 const isInteracting = computed(() => interactions.isInteracting)
 
 // Template refs
-const parentRef = ref()
-const domElement = ref()
+const innerPanArea = ref()
+const outerPanArea = ref() // The parent DOM Node
 const panzoomInstance = ref<PanzoomObject | undefined>()
 
 // Watch for changes and change Panzoom accordingly
@@ -58,39 +62,37 @@ watch(panzoomEnabled, (newVal) => {
 })
 
 onMounted(async () => {
-  // Initialize
-  // domElement.value = parentRef.value
-  domElement.value = document.querySelector('#canvas')
-
   // TODO: This should be refactored after Vue 3 update
   const { $root } = getCurrentInstance()?.proxy
   if (!$root) console.warn('No Panzoom created')
 
   // Init Panzoom globally
-  $root.$panzoom = Panzoom(domElement.value, {
+  // We use the 'canvas' option to enable interactions on the parent DOM Node as well
+  // Docs: https://github.com/timmywil/panzoom#canvas
+  $root.$panzoom = Panzoom(innerPanArea.value, {
     canvas: true, // Allows parent to control child
     cursor: 'grab',
     startScale: 1,
     startX: 0,
-    // Scale to fit the content
-    handleStartEvent: (event: Event) => {
-      // Default actions
+    handleStartEvent: (event: PointerEvent) => {
       // WARNING: Don't use preventDefault, as it will block other events
-
       // event.preventDefault()
 
-      if (!panzoomEnabled)
-        console.warn('Panzoom started although it should be disabled')
+      // This event (pointerdown/mousedown) can often fire _before_ other mousedown events!
+      console.log('panzoom start event')
 
-      // console.log('Panzooming logic')
-      // if (!panzoomEnabled || userEventsState.canDragSelect) {
-      //   // event.preventDefault() // default
-      //   disablePanzoom()
-      //   return false
-      // } else {
-      //   enablePanzoom()
-      // }
-      // console.log(panzoomEnabled, userEventsState.canDragSelect)
+      // Check if user has CTRL/CMD key selected
+      // If so, we can assume they don't want to Panzoom
+      if (userEventsState.isCtrlPressed) {
+        disablePanzoom()
+        return false
+      }
+
+      if (!panzoomEnabled) {
+        disablePanzoom()
+      } else {
+        enablePanzoom()
+      }
     },
   })
 
@@ -115,33 +117,45 @@ function enablePanzoom() {
     disablePan: false,
     disableZoom: false,
   })
+
+  interactions.setPanzoomState(true)
+
+  interactions.interactionSetState({
+    key: 'isPanzooming',
+    value: true,
+  })
 }
 function disablePanzoom() {
+  console.log('panzoom disabled')
   panzoomInstance.value?.setOptions({
     disablePan: true,
     disableZoom: true,
+  })
+
+  interactions.setPanzoomState(false)
+
+  interactions.interactionSetState({
+    key: 'isPanzooming',
+    value: false,
   })
 }
 
 function enableEventListeners() {
   const instance = panzoomInstance.value
-  const element = domElement.value
 
   // element.addEventListener("panzoomchange", event => {
   //   console.log(event.detail); // => { x: 0, y: 0, scale: 1 }
   // });
 
-  // Enable when CMD is not pressed
-  // useEventListener(window, 'keydown', this.cmdHandler)
-  // useEventListener(window, 'keyup', this.cmdHandler)
-
   // TODO Add tests for these
 
+  // IMPORTANT: We bind the listeners to the container
+
   // Handle mouse & touch events
-  mouseHandlers(element, instance)
+  mouseHandlers(outerPanArea.value, instance)
 
   // Mousewheel zoom w/ CMD/CTRL key
-  wheelHandler(element, instance)
+  wheelHandler(outerPanArea.value, instance)
 
   // Listen for menu bar events
   if (isElectron()) {
@@ -160,10 +174,12 @@ function enableEventListeners() {
  * @param instance The Panzoom instance
  */
 function wheelHandler(DOMElement, instance) {
-  DOMElement.parentElement.addEventListener('wheel', onWheel)
+  DOMElement.addEventListener('wheel', onWheel)
 
   function onWheel(event) {
     if (!panzoomEnabled) return false // Only do this if Panzoom is enabled
+
+    console.log('panzoom wheel')
 
     // Prevent default scroll event
     event.preventDefault()
@@ -193,19 +209,18 @@ function wheelHandler(DOMElement, instance) {
  */
 function mouseHandlers(DOMElement, instance) {
   // TODO These DO NOT WORK
-  const parentElement = DOMElement.parentElement
 
   // Emit start events
-  const onEvents = ['mousedown', 'touchstart', 'gesturestart']
-  onEvents.forEach((name) => {
-    parentElement.addEventListener(name, startEvents)
-  })
+  // const onEvents = ['mousedown', 'touchstart', 'gesturestart']
+  // onEvents.forEach((name) => {
+  //   DOMElement.addEventListener(name, startEvents)
+  // })
 
-  const offEvents = ['mouseup', 'touchend', 'gestureend']
-  offEvents.forEach((name) => {
-    parentElement.removeEventListener(name, startEvents)
-    parentElement.addEventListener(name, endEvents)
-  })
+  // const offEvents = ['mouseup', 'touchend', 'gestureend']
+  // offEvents.forEach((name) => {
+  //   DOMElement.removeEventListener(name, startEvents)
+  //   DOMElement.addEventListener(name, endEvents)
+  // })
 
   function startEvents(e) {
     // Only continue if Panzoom is enabled
@@ -236,7 +251,7 @@ function mouseHandlers(DOMElement, instance) {
     })
 
     // TODO: Remove event listener for just this one event name
-    parentElement.removeEventListener(name, endEvents)
+    DOMElement.removeEventListener(name, endEvents)
   }
 }
 
