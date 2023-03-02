08aa1884a2f8fdf872e22606d180d884c3749707
Nick Wittwer <nick.wittwer@me.com>
2023-03-02 17:32:57 +0100

    Fix/release bumping (#237)
    * Found solution to running TS file with Node in place (package.json release script with esrun, ts-config, updater-cli.ts)
    
    * v0.7.0-beta.27
    
    * Use fs.readfile instead of unpredictable await import of package.json file
    
    * Improved prompt copywriting and fixed newVersion variable to grab the correct new value

--- app/scripts/updater-cli.ts
+++ app/scripts/updater-cli.ts
@@ -1,12 +1,12 @@
-import { fileURLToPath } from 'url'
+// import { fileURLToPath } from 'url'
 import inquirer from 'inquirer'
 import path from 'path'
 import { execa, execaCommand } from 'execa'
 import { version as packageVersion } from '../package.json'
+import fs from 'fs/promises'
+import cloneDeep from 'lodash/cloneDeep'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const initialPath = path.resolve(__dirname)
-const pathToApp = path.resolve(__dirname, '../') // Absolute path
+const pathToApp = path.resolve(__dirname, '../') // Path to the Electron app
 
 const main = async () => {
   console.info(`Local app version is: ${packageVersion}`)
@@ -21,20 +21,27 @@ const main = async () => {
 main()
 
 async function execaAtRoot(shellString: string, args: any[], options?: {}) {
-  const { stdout } = await execa(shellString, ...args, {
-    ...options,
+  return await execa(shellString, ...args, {
     cwd: pathToApp,
+    // stdio: 'inherit', // Pipe out to main process
+    shell: true,
+    ...options,
   }).catch((err) => {
     console.error(err)
+    return false
   })
-
-  return {
-    stdout,
-  }
 }
 
 async function execaCommandAtRoot(command: string, options?: {}) {
-  return await execaCommand(command, options)
+  return await execaCommand(command, {
+    // stdio: 'inherit', // Pipe out to main process
+    cwd: pathToApp,
+    shell: true,
+    ...options,
+  }).catch((err) => {
+    console.error(err)
+    return false
+  })
 }
 
 async function inquireUpdateType() {
@@ -47,11 +54,6 @@ async function inquireUpdateType() {
       default: 'prerelease',
     })
     .then(async (answers) => {
-      // answers.updateType
-
-      // CD to app directory
-      // shell.cd(pathToApp)
-
       ////////////////////////////////
       // Check if the current package.json version
       // exists at Git origin
@@ -60,35 +62,58 @@ async function inquireUpdateType() {
       // TODO: Check via Github API?
 
       ////////////////////////////////
-      // Bump the version
-      try {
-        console.log(`Bumping ${answers.updateType}...`)
-        await execaAtRoot('yarn', ['version ${answers.updateType}'])
-        console.log('Version bumped!')
-      } catch (err) {
-        // Handle cases where Yarn version plugin is not installed: "yarn plugin import version"
-        console.error(err)
+      // Bump the version w/ Yarn CLI
+      // Docs: https://yarnpkg.com/cli/version
+      console.log(`Bumping ${answers.updateType}...`)
+
+      // We have to manually remove the .stableVersion value that Yarn sets
+      // so that bumping will take place
+      // Bug: https://github.com/yarnpkg/berry/issues/4328#issue-1194658629
+      await removeStableVersion()
+
+      // Use Yarn to bump the version
+      // --immediate: We will immediately bump the version in package.json, commit the change
+      // and tag the version with Git
+      await execaCommandAtRoot(`yarn version ${answers.updateType}`)
+
+      // Check updated package.json version
+      const updatedPackageVersion = await fs
+        .readFile(path.resolve(pathToApp, 'package.json'), 'utf-8')
+        .then((data) => {
+          const { version } = JSON.parse(data)
+          return version
+        })
+
+      // Version with "v" prefix
+      // Example: v0.7.0
+      const newVersion = 'v' + updatedPackageVersion
+
+      // Ensure that the version has really been bumped
+      const hasChanged = packageVersion !== updatedPackageVersion
+      if (!hasChanged) {
+        console.error(
+          'Error: Version not bumped!',
+          packageVersion,
+          updatedPackageVersion
+        )
+        process.exit()
       }
 
-      // package.json version will be changed by Yarn
-      // Get updated version
-      const newVersion = 'v' + packageVersion
+      console.log(`Version bumped! to ${updatedPackageVersion}`)
 
-      // Pull from latest tags
+      ////////////////////////////////
+      // Get tags & create new tag
+
+      // --prune: connect to remote repository and fetch all remote branch refs -> delete remote refs that are no longer in use on remote repository
       console.log(`Getting latest tags`)
-      await execaAtRoot('git', ['fetch --tags --all --prune'])
+      await execaCommandAtRoot('git fetch --tags --all --prune')
 
-      // Check if existing tag
-
-      const tags = await execaAtRoot('git', ['tag --list']).catch((err) =>
-        console.error(err)
-      )
-
-      // Git Commit
-      console.log(`Committing`)
-      await execaCommandAtRoot(
-        `git add package.json && git commit -m ${newVersion}`
-      )
+      // Get existing tags
+      const { stdout: tags } = await execaCommandAtRoot('git tag --list')
+      if (!tags) {
+        console.error('No tags found!')
+        return false
+      }
 
       // Ask if user wants to replace existing tag
       if (tags.includes(newVersion)) {
@@ -103,15 +128,41 @@ async function inquireUpdateType() {
             if (confirmDeleteTag === true) {
               await deleteGitTag(newVersion) // Remove existing tag
             } else {
-              console.log(`${newVersion} tag not replaced`)
+              console.log(`${newVersion} tag not replaced. Exiting.`)
               process.exit() // Stop the script
             }
           })
       }
 
-      // Create Git tag
-      console.log(`Tagging`)
-      await execaCommandAtRoot(`git tag ${newVersion}`)
+      // Git Commit
+      await inquirer
+        .prompt({
+          name: 'confirmCommit',
+          type: 'confirm',
+          message: `⚠️ Ready to commit the package.json and Git tag the version?`,
+          default: false,
+        })
+        .then(async ({ confirmCommit }) => {
+          if (confirmCommit === true) {
+            // Git commit the package.json with the commit named as the version
+            // NOTE: This will trigger the CI build
+            console.log(`Committing`)
+            await execaCommandAtRoot(
+              `git add package.json && git commit -m ${newVersion}`
+            ).catch((err) => {
+              console.error('custom err')
+            })
+
+            // Create Git tag
+            console.log(`Tagging`)
+            await execaCommandAtRoot(`git tag ${newVersion}`)
+          } else {
+            console.log(
+              `${newVersion} not committed and tag not created. Exiting.`
+            )
+            process.exit() // Stop the script
+          }
+        })
 
       ////////////////////////////////
 
@@ -120,7 +171,7 @@ async function inquireUpdateType() {
         .prompt({
           name: 'confirmUpdate',
           type: 'confirm',
-          message: `Do you want to Git push ${newVersion} to origin? This will also push any other other local commits, and a release build will start.`,
+          message: `Ready to push ${newVersion} to origin? This will also push any other other local commits, and a release build will start.`,
           default: false,
         })
         .then(async ({ confirmUpdate }) => {
@@ -136,13 +187,28 @@ async function inquireUpdateType() {
             )
           }
         })
-
-      // Return too root
-      // shell.cd(initialPath)
     })
 }
 
 async function deleteGitTag(tagName) {
   await execaCommandAtRoot(`git push --delete origin ${tagName}`) // Remove existing tag on origin
   await execaCommandAtRoot(`git tag -d ${tagName}`) // Remove existing tag locally
+}
+
+async function removeStableVersion() {
+  const packageJsonPath = path.resolve(pathToApp, 'package.json')
+
+  const { default: currPackageJson } = await import(packageJsonPath, {
+    assert: {
+      type: 'json',
+    },
+  })
+  const newPackageJson = cloneDeep(currPackageJson)
+
+  // Remove the .stableVersion key
+  delete newPackageJson.stableVersion
+
+  // Write the file back
+  // TODO: Add async
+  await fs.writeFile(packageJsonPath, JSON.stringify(newPackageJson, null, 2))
 }
