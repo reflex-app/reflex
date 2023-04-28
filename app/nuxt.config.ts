d9d3bfe572b4ab3a208ecf8c63dd6e24d2356ce6
Nick Wittwer <6360459+nwittwer@users.noreply.github.com>
2023-04-28 20:37:43 +0200

    Added vite-plugin-electron-renderer to be able to use Node modules within renderer (Nuxt)

---
+++ app/nuxt.config.ts
@@ -0,0 +1,86 @@
+// import vuetify from 'vite-plugin-vuetify'
+import renderer from 'vite-plugin-electron-renderer'
+import type { ElectronOptions } from 'nuxt-electron'
+
+const isDevMode = process.env.NODE_ENV !== 'production'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  components: false,
+  router: {
+    options: {
+      hashMode: true,
+    },
+  },
+  vite: {
+    server: {
+      middlewareMode: false,
+    },
+    plugins: [
+      renderer({
+        nodeIntegration: true,
+      }),
+    ],
+  },
+  // Environment variables
+  runtimeConfig: {
+    public: {
+      DEV: isDevMode,
+    },
+  },
+
+  // TODO: Considering moving all Nuxt files to a subfolder
+  // srcDir: './nuxt/',
+  app: {
+    baseURL: './',
+  },
+  modules: [
+    // async (options, nuxt) => {
+    //   // @ts-expect-error: remove ts error
+    //   nuxt.hooks.hook('vite:extendConfig', (config) =>
+    //     config.plugins.push(vuetify())
+    //   )
+    // },
+    // Nuxt + Electron configuration
+    // Docs: https://github.com/caoxiemeihao/nuxt-electron
+    // Example: https://github.com/gurvancampion/nuxt-electron-trpc-prisma/
+    [
+      'nuxt-electron',
+      <ElectronOptions>{
+        include: ['electron', 'server', 'extraResources'],
+        // root: __dirname + '/nuxt',
+      },
+    ],
+    '@pinia/nuxt', // Use Pinia w/ Nuxt 3
+    '@pinia-plugin-persistedstate/nuxt', // Persisted state w/ Pinia + Nuxt (Nuxt 3)
+  ],
+  // Vue settings
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => ['webview'].includes(tag),
+    },
+  },
+  // Pinia Store <> localStorage persistence
+  piniaPersistedstate: {
+    storage: 'localStorage', // Use localStorage as default storage
+  },
+  hooks: {
+    // Remove aliases to only have one
+    // https://github.com/nuxt/framework/issues/7277
+    'prepare:types': function ({ tsConfig }) {
+      const aliasesToRemoveFromAutocomplete = [
+        '~~',
+        '~~/*',
+        '@',
+        '@/*',
+        '@@',
+        '@@/*',
+      ]
+      for (const alias of aliasesToRemoveFromAutocomplete) {
+        if (tsConfig.compilerOptions!.paths[alias])
+          delete tsConfig.compilerOptions!.paths[alias]
+      }
+    },
+  },
+})
