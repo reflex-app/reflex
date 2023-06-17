70cf3603e5a90f1ff57d5c656b8ba600ac5d7831
Nick Wittwer <6360459+nwittwer@users.noreply.github.com>
2023-06-17 09:38:38 +0200

    Changed styles of SiteTree to be a side panel

--- app/components/SiteTree/index.vue
+++ app/components/SiteTree/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div v-if="isVisible"
-    class="absolute z-[1000] top-0 left-0 bg-white/80 w-full h-full flex flex-wrap place-content-center" @click="close()">
-    <div class="h-max max-h-screen max-w-3xl w-full bg-white rounded-lg p-16 shadow-xl overflow-auto" @click.stop>
+  <div id="site-tree-container" v-show="isVisible">
+    <div
+      class="h-max max-h-screen max-w-3xl w-full bg-white overflow-auto"
+    >
       <h1 class="text-4xl mb-4">Sites</h1>
       <Tree />
     </div>
@@ -10,16 +11,19 @@
 
 <script setup lang="ts">
 import { useGuiStore } from '~/store/gui'
-import Tree from './childUrl.vue';
+import Tree from './childUrl.vue'
 
 const gui = useGuiStore()
 const isVisible = computed(() => gui.siteTree)
-
-const close = () => {
-  gui.toggleGui('siteTree')
-}
 </script>
 
-<style scoped>
-/* Add any custom styles here */
+<style scoped lang="scss">
+@import '~/scss/_variables';
+
+#site-tree-container {
+  min-height: calc(100vh - #{$gui-title-bar-height});
+  border-left: 1px solid $border-color;
+  @apply absolute z-10 top-0 right-0 bg-white w-[650px] h-full flex flex-wrap p-4;
+  // @apply absolute z-[1000] top-0 left-0 bg-white/80 w-full h-full flex flex-wrap place-content-center;
+}
 </style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="main-view">
     <DevModeHud />
-    <SiteTree />
     <SidePanel />
+    <SiteTree />
     <Screenshots />
     <Panzoom>
       <Artboards />
